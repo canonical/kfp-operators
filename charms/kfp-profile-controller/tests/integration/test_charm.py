a050df5d7533694d4e7f9757fbda5b485d464bf8
Andrew Scribner <ca.scribner+1@gmail.com>
2021-09-24 15:41:28 -0400

    docs: add notes for future tests

--- charms/kfp-profile-controller/tests/integration/test_charm.py
+++ charms/kfp-profile-controller/tests/integration/test_charm.py
@@ -36,3 +36,14 @@ async def test_build_and_deploy(ops_test: OpsTest):
     # TODO: Need a better test for checking everything is ok
     # Maybe: await ops_test.model.wait_for_idle(raise_on_error=False, raise_on_blocked=True) or similar?
     await ops_test.model.wait_for_idle(timeout=60 * 10)
+
+
+# TODO: Real testing requires:
+#  * kubeflow-profile controller, or something that will make namespaces/service accounts in a
+#    similar fashion (some resources deployed to each namespace by kfp-profile-controller use the
+#    `default-editor` service account, which is created by the main kubeflow-profile controller)
+#  * This charm deploying successfully is not a full test of function.  We need to create a new
+#    tracked namespace (namespace that has label pipelines.kubeflow.org/enabled=true) and ensure all
+#    expected resources are deployed and come up successfully (eg: if service account is not found,
+#    deployments will exist but pods will never be "ready")
+
