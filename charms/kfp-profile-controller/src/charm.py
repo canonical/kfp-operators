3ed0b4980b996c983c088a5c8f51946af43b808d
Andrew Scribner <ca.scribner+1@gmail.com>
2021-09-24 13:07:02 -0400

    feat: complete adding the kfp-profile-controller
    This commit appears to be in a MVP state.  The charm will deploy successfully and reach active/idle.  Additional testing is needed
    
    Included here are some integration tests and missing pieces from previous commit.

--- charms/kfp-profile-controller/src/charm.py
+++ charms/kfp-profile-controller/src/charm.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import json
 import logging
 from pathlib import Path
 
@@ -53,10 +52,10 @@ class Operator(CharmBase):
                 self.on[relation].relation_changed,
                 self.set_pod_spec,
             )
-            self.framework.observe(
-                self.on[relation].relation_changed,
-                self.send_info,
-            )
+            # self.framework.observe(
+                # self.on[relation].relation_changed,
+                # self.send_info,
+            # )
 
     # def send_info(self, event):
     #     if self.interfaces["kfp-api"]:
@@ -77,22 +76,28 @@ class Operator(CharmBase):
 
         config = self.model.config
 
+        # TODO: this didn't seem to work.  if deployed before object storage is up, still goes
+        #  to active state
         if not ((os := self.interfaces["object-storage"]) and os.get_data()):
             self.model.unit.status = WaitingStatus(
                 "Waiting for object-storage relation data"
             )
             return
+
+        logging.info("Found all required relations - proceeding to setting pod spec")
+        self.model.unit.status = MaintenanceStatus("Setting pod spec")
         os = list(os.get_data().values())[0]
 
         deployment_env = {
             "MINIO_ACCESS_KEY": os["access-key"],
             "MINIO_SECRET_KEY": os["secret-key"],
-            # "KFP_VERSION": ___,  # optional
+            # TODO: This sets the version of the images used in each namespace, coming from the
+            #  configMap pipeline-install-config's appVersion entry.  Should this be a config option?  It'll be updated whenever we update the charm
+            "KFP_VERSION": "1.7.0-rc.3",
             "KFP_DEFAULT_PIPELINE_ROOT": "",
             "DISABLE_ISTIO_SIDECAR": "false",
         }
 
-        self.model.unit.status = MaintenanceStatus("Setting pod spec")
         self.model.pod.set_spec(
             {
                 "version": 3,
@@ -101,15 +106,15 @@ class Operator(CharmBase):
                         # TODO: annotations: sidecar.istio.io/inject: "false"
                         # TODO: labels?
                         "name": "kubeflow-pipelines-profile-controller",
-                        "command": "python",
-                        "args": ["/hooks/s.py"],
+                        "command": ["python"],
+                        "args": ["/hooks/sync.py"],
                         "imageDetails": image_details,
                         "ports": [
                             {
                                 "name": "http",
                                 # TODO: Upstream maps kubeflow maps 80->8080.  This this is fine?
                                 "containerPort": int(config["http-port"]),
-                                "protocol": "TCP"
+                                "protocol": "TCP",
                             },
                         ],
                         "envConfig": deployment_env,
@@ -117,95 +122,87 @@ class Operator(CharmBase):
                             {
                                 "name": "kubeflow-pipelines-profile-controller-code",
                                 "mountPath": "/hooks",
-                                "files": [
-                                    {
-                                        "key": "sync.py",
-                                        "path": "sync.py",
-                                    }
-                                ],
-                            }
+                                "configMap": {
+                                    "name": "kubeflow-pipelines-profile-controller-code",
+                                    "files": [
+                                        {
+                                            "key": "sync.py",
+                                            "path": "sync.py",
+                                        },
+                                    ],
+                                },
+                            },
                         ],
-                    }
+                    },
                 ],
             },
             k8s_resources={
                 "kubernetesResources": {
                     "customResources": {
-                        "ompositecontrollers.metacontroller.k8s.io": {
-                            "apiVersion": "metacontroller.k8s.io/v1alpha1",
-                            "kind": "CompositeController",
-                            "metadata": {
-                                "name": "kubeflow-pipelines-profile-controller",
-                                # "labels": {}, # TODO: Include labels?  think they might be defined at metadata indentation
-                            },
-                            "spec": {
-                                "childResources": [
-                                    {
-                                        "apiVersion": "v1",
-                                        "resource": "secrets",
-                                        "updateStrategy": {
-                                            "method": "OnDelete"
+                        "compositecontrollers.metacontroller.k8s.io": [
+                            {
+                                "apiVersion": "metacontroller.k8s.io/v1alpha1",
+                                "kind": "CompositeController",
+                                "metadata": {
+                                    "name": "kubeflow-pipelines-profile-controller",
+                                    # "labels": {}, # TODO: Include labels?  think they might be defined at metadata indentation
+                                },
+                                "spec": {
+                                    "childResources": [
+                                        {
+                                            "apiVersion": "v1",
+                                            "resource": "secrets",
+                                            "updateStrategy": {"method": "OnDelete"},
                                         },
+                                        {
+                                            "apiVersion": "v1",
+                                            "resource": "configmaps",
+                                            "updateStrategy": {"method": "OnDelete"},
+                                        },
+                                        {
+                                            "apiVersion": "apps/v1",
+                                            "resource": "deployments",
+                                            "updateStrategy": {"method": "InPlace"},
+                                        },
+                                        {
+                                            "apiVersion": "v1",
+                                            "resource": "services",
+                                            "updateStrategy": {"method": "InPlace"},
+                                        },
+                                        {
+                                            "apiVersion": "networking.istio.io/v1alpha3",
+                                            "resource": "destinationrules",
+                                            "updateStrategy": {"method": "InPlace"},
+                                        },
+                                        {
+                                            "apiVersion": "security.istio.io/v1beta1",
+                                            "resource": "authorizationpolicies",
+                                            "updateStrategy": {"method": "InPlace"},
+                                        },
+                                    ],
+                                    "generateSelector": True,
+                                    "hooks": {
+                                        "sync": {
+                                            "webhook": {
+                                                "url": "http://kubeflow-pipelines-profile-controller/sync"  # This needs to match the service juju makes for us
+                                            }
+                                        }
                                     },
-                                    {
+                                    "parentResource": {
                                         "apiVersion": "v1",
-                                        "resource": "configmaps",
-                                        "updateStrategy": {
-                                            "method": "OnDelete"
-                                        }
+                                        "resource": "namespaces",
                                     },
-                                    {
-                                        "apiVersion": "apps/v1",
-                                        "resource": "deployments",
-                                        "updateStrategy": {
-                                            "method": "InPlace"
-                                        }
-                                    },
-                                    {
-                                        "apiVersion": "v1",
-                                        "resource": "services",
-                                        "updateStrategy": {
-                                            "method": "InPlace"
-                                        }
-                                    },
-                                    {
-                                        "apiVersion": "networking.istio.io/v1alpha3",
-                                        "resource": "destinationrules",
-                                        "updateStrategy": {
-                                            "method": "InPlace"
-                                        }
-                                    },
-                                    {
-                                        "apiVersion": "security.istio.io/v1beta1",
-                                        "resource": "authorizationpolicies",
-                                        "updateStrategy": {
-                                            "method": "InPlace"
-                                        }
-                                    },
-                                ],
-                                "generateSelector": "true",
-                                "hooks": {
-                                    "sync": {
-                                        "webhook": {
-                                            "url": "http://kubeflow-pipelines-profile-controller/sync"  # This needs to match the service juju makes for us
-                                        }
-                                    }
+                                    "resyncPeriodSeconds": 10,
                                 },
-                                "parentResource": {
-                                    "apiVersion": "v1",
-                                    "resource": "namespaces",
-                                },
-                                "resyncPeriodSeconds": 10,
                             }
-                        }
+                        ]
                     },
-                    "configMaps": {
-                        "kubeflow-pipelines-profile-controller-code": {
-                            "sync.py": Path("files/upstream/sync.py"),
-                        },
+                },
+                "configMaps": {
+                    "kubeflow-pipelines-profile-controller-code": {
+                        "sync.py": Path("files/upstream/sync.py").read_text(),
                     },
-
-                }
+                },
             },
         )
         self.model.unit.status = ActiveStatus()
