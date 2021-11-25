65620ccc7589c91b73f36cc9ee331c78a4d2ec54
Andrew Scribner <ca.scribner+1@gmail.com>
2021-11-25 16:43:50 -0500

    feat: parametrize mysql tests

--- charms/kfp-api/tests/unit/test_operator.py
+++ charms/kfp-api/tests/unit/test_operator.py
@@ -33,45 +33,65 @@ def test_image_fetch(harness, oci_resource_data):
 
 
 # Tests to do:
-# * required relations missing block things
-#   * object-storage
 # * things happen when we have required
 # * subscriber relations are optional, but they provide the expected data
-# * where else can I look for tests?  notebook?  minio?
 # * config_changed actually changes something?
 # Test when SOMETHING raises CheckFailed that it hits the status properly.  Or, check it for everything (parameterize somehow?)
 
 
-def test_mysql_relation(harness):
+@pytest.mark.parametrize(
+    "relation_data,expected_returned_data,expected_raises,expected_status",
+    (
+        (
+            # No relation established.  Raises CheckFailed
+            None,
+            None,
+            pytest.raises(CheckFailed),
+            BlockedStatus("Missing required relation for mysql"),
+        ),
+        (
+            # Relation exists but no data posted yet
+            {},
+            None,
+            pytest.raises(CheckFailed),
+            WaitingStatus("Waiting for mysql relation data"),
+        ),
+        (
+            # Relation exists with only partial data
+            {"database": "database"},
+            None,
+            pytest.raises(CheckFailed),
+            BlockedStatus("Received incomplete data from mysql relation.  See logs"),
+        ),
+        (
+            # Relation complete
+            {
+                "database": "database",
+                "host": "host",
+                "root_password": "root_password",
+                "port": "port",
+            },
+            {
+                "database": "database",
+                "host": "host",
+                "root_password": "root_password",
+                "port": "port",
+            },
+            does_not_raise(),
+            None,
+        ),
+    ),
+)
+def test_mysql_relation(
+    harness, relation_data, expected_returned_data, expected_raises, expected_status
+):
     harness.begin()
-
-    # Test no relation
-    with pytest.raises(CheckFailed) as no_relation:
-        harness.charm._get_mysql()
-    assert no_relation.value.status == BlockedStatus(
-        "Missing required relation for mysql"
-    )
 
     mysql_app = "mysql_app"
     mysql_unit = f"{mysql_app}/0"
 
-    # Test relation with missing data
     rel_id = harness.add_relation("mysql", mysql_app)
     harness.add_relation_unit(rel_id, mysql_unit)
-    with pytest.raises(CheckFailed) as no_relation_data:
-        harness.charm._get_mysql()
-    assert no_relation_data.value.status == WaitingStatus(
-        "Waiting for mysql relation data"
-    )
-
-    # Test with partial data
-    data = {"database": "database"}
-    harness.update_relation_data(rel_id, mysql_unit, data)
-    with pytest.raises(CheckFailed) as partial_relation_data:
-        harness.charm._get_mysql()
-    assert partial_relation_data.value.status == BlockedStatus(
-        "Received incomplete data from mysql relation.  See logs"
-    )
 
     # Test complete relation
     data = {
@@ -84,23 +104,27 @@ def test_mysql_relation(harness):
     with does_not_raise():
         harness.charm._get_mysql()
 
-    # Test too many relations
+
+def test_mysql_relation_too_many_relations(harness):
+    harness.begin()
+
+    mysql_app = "mysql_app"
+    mysql_unit = f"{mysql_app}/0"
+
+    rel_id = harness.add_relation("mysql", mysql_app)
+    harness.add_relation_unit(rel_id, mysql_unit)
     rel_id_2 = harness.add_relation("mysql", "extra_sql")
     harness.add_relation_unit(rel_id_2, "extra_sql/0")
+
     with pytest.raises(CheckFailed) as too_many_relations:
         harness.charm._get_mysql()
     assert too_many_relations.value.status == BlockedStatus("Too many mysql relations")
-
-
-def test_too_many_mysql_relation(harness):
-    assert False
 
 
 def test_kfp_viz_relation_missing(harness):
     harness.set_leader()
     harness.begin()
 
-    viz_app = "kfp-viz"
     default_viz_data = {"service-name": "unset", "service-port": "1234"}
 
     # Could mock this away, but looked complicated
@@ -189,59 +213,53 @@ def test_kfp_viz_relation(
 @pytest.mark.parametrize(
     "relation_data,expected_returned_data,expected_raises,expected_status",
     (
-            # No relation established.  Raises CheckFailed
-            (
-                    None,
-                    None,
-                    pytest.raises(CheckFailed),
-                    BlockedStatus("Missing required relation for object-storage"),
+        # No relation established.  Raises CheckFailed
+        (
+            None,
+            None,
+            pytest.raises(CheckFailed),
+            BlockedStatus("Missing required relation for object-storage"),
+        ),
+        (
+            # Relation exists but no versions set yet
+            {},
+            None,
+            pytest.raises(CheckFailed),
+            WaitingStatus("List of object-storage versions not found for apps: minio"),
+        ),
+        (
+            # Relation exists with versions, but no data posted yet
+            {"_supported_versions": "- v1"},
+            None,
+            pytest.raises(CheckFailed),
+            WaitingStatus("Waiting for object-storage relation data"),
+        ),
+        (
+            # Relation exists with versions and empty data
+            {"_supported_versions": "- v1", "data": yaml.dump({})},
+            None,
+            pytest.raises(CheckFailed),
+            BlockedStatus(
+                "Found incomplete/incorrect relation data for object-storage."
             ),
-            (
-                    # Relation exists but no versions set yet
-                    {},
-                    None,
-                    pytest.raises(CheckFailed),
-                    WaitingStatus("List of object-storage versions not found for apps: minio"),
+        ),
+        (
+            # Relation exists with versions and invalid (partial) data
+            {
+                "_supported_versions": "- v1",
+                "data": yaml.dump({"service-name": "service-name"}),
+            },
+            None,
+            pytest.raises(CheckFailed),
+            BlockedStatus(
+                "Found incomplete/incorrect relation data for object-storage.  See logs"
             ),
-            (
-                    # Relation exists with versions, but no data posted yet
-                    {"_supported_versions": "- v1"},
-                    None,
-                    pytest.raises(CheckFailed),
-                    WaitingStatus("Waiting for object-storage relation data"),
-            ),
-            (
-                    # Relation exists with versions and empty data
-                    {"_supported_versions": "- v1", "data": yaml.dump({})},
-                    None,
-                    pytest.raises(CheckFailed),
-                    BlockedStatus("Found incomplete/incorrect relation data for object-storage."),
-            ),
-            (
-                    # Relation exists with versions and invalid (partial) data
-                    {
-                        "_supported_versions": "- v1",
-                        "data": yaml.dump({"service-name": "service-name"}),
-                    },
-                    None,
-                    pytest.raises(CheckFailed),
-                    BlockedStatus(
-                        "Found incomplete/incorrect relation data for object-storage.  See logs"
-                    ),
-            ),
-            (
-                    # Relation exists with valid data
-                    {
-                        "_supported_versions": "- v1",
-                        "data": yaml.dump({
-                            "access-key": "access-key",
-                            "namespace": "namespace",
-                            "port": 1234,
-                            "secret-key": "secret-key",
-                            "secure": True,
-                            "service": "service",
-                        }),
-                    },
+        ),
+        (
+            # Relation exists with valid data
+            {
+                "_supported_versions": "- v1",
+                "data": yaml.dump(
                     {
                         "access-key": "access-key",
                         "namespace": "namespace",
@@ -249,14 +267,24 @@ def test_kfp_viz_relation(
                         "secret-key": "secret-key",
                         "secure": True,
                         "service": "service",
-                    },
-                    does_not_raise(),
-                    None,
-            ),
+                    }
+                ),
+            },
+            {
+                "access-key": "access-key",
+                "namespace": "namespace",
+                "port": 1234,
+                "secret-key": "secret-key",
+                "secure": True,
+                "service": "service",
+            },
+            does_not_raise(),
+            None,
+        ),
     ),
 )
 def test_object_storage_relation(
-        harness, relation_data, expected_returned_data, expected_raises, expected_status
+    harness, relation_data, expected_returned_data, expected_raises, expected_status
 ):
     harness.set_leader()
     harness.begin()
