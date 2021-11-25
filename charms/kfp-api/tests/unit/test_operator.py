b3f5692c47b3c10308d064fe9a7678628fb2fd17
Andrew Scribner <ca.scribner+1@gmail.com>
2021-11-25 16:30:16 -0500

    feat: add object-storage relation tests

--- charms/kfp-api/tests/unit/test_operator.py
+++ charms/kfp-api/tests/unit/test_operator.py
@@ -109,7 +109,7 @@ def test_kfp_viz_relation_missing(harness):
 
 
 @pytest.mark.parametrize(
-    "relation_data,expected_viz_data,expected_raises,expected_status",
+    "relation_data,expected_returned_data,expected_raises,expected_status",
     (
         # No relation established.  Returns default value
         (
@@ -164,7 +164,7 @@ def test_kfp_viz_relation_missing(harness):
     ),
 )
 def test_kfp_viz_relation(
-    harness, relation_data, expected_viz_data, expected_raises, expected_status
+    harness, relation_data, expected_returned_data, expected_raises, expected_status
 ):
     harness.set_leader()
     harness.begin()
@@ -181,17 +181,102 @@ def test_kfp_viz_relation(
         interfaces = harness.charm._get_interfaces()
         viz = harness.charm._get_viz(interfaces)
     if expected_status is None:
-        assert viz == expected_viz_data
+        assert viz == expected_returned_data
     else:
         assert partial_relation_data.value.status == expected_status
 
 
-def test_object_storage_relation(harness):
+@pytest.mark.parametrize(
+    "relation_data,expected_returned_data,expected_raises,expected_status",
+    (
+            # No relation established.  Raises CheckFailed
+            (
+                    None,
+                    None,
+                    pytest.raises(CheckFailed),
+                    BlockedStatus("Missing required relation for object-storage"),
+            ),
+            (
+                    # Relation exists but no versions set yet
+                    {},
+                    None,
+                    pytest.raises(CheckFailed),
+                    WaitingStatus("List of object-storage versions not found for apps: minio"),
+            ),
+            (
+                    # Relation exists with versions, but no data posted yet
+                    {"_supported_versions": "- v1"},
+                    None,
+                    pytest.raises(CheckFailed),
+                    WaitingStatus("Waiting for object-storage relation data"),
+            ),
+            (
+                    # Relation exists with versions and empty data
+                    {"_supported_versions": "- v1", "data": yaml.dump({})},
+                    None,
+                    pytest.raises(CheckFailed),
+                    BlockedStatus("Found incomplete/incorrect relation data for object-storage."),
+            ),
+            (
+                    # Relation exists with versions and invalid (partial) data
+                    {
+                        "_supported_versions": "- v1",
+                        "data": yaml.dump({"service-name": "service-name"}),
+                    },
+                    None,
+                    pytest.raises(CheckFailed),
+                    BlockedStatus(
+                        "Found incomplete/incorrect relation data for object-storage.  See logs"
+                    ),
+            ),
+            (
+                    # Relation exists with valid data
+                    {
+                        "_supported_versions": "- v1",
+                        "data": yaml.dump({
+                            "access-key": "access-key",
+                            "namespace": "namespace",
+                            "port": 1234,
+                            "secret-key": "secret-key",
+                            "secure": True,
+                            "service": "service",
+                        }),
+                    },
+                    {
+                        "access-key": "access-key",
+                        "namespace": "namespace",
+                        "port": 1234,
+                        "secret-key": "secret-key",
+                        "secure": True,
+                        "service": "service",
+                    },
+                    does_not_raise(),
+                    None,
+            ),
+    ),
+)
+def test_object_storage_relation(
+        harness, relation_data, expected_returned_data, expected_raises, expected_status
+):
     harness.set_leader()
     harness.begin()
 
-    interfaces = harness.charm._get_interfaces()
-    os = harness.charm._get_object_storage(interfaces)
+    relation_name = "object-storage"
+    other_app = "minio"
+    other_unit = f"{other_app}/0"
+
+    if relation_data is not None:
+        rel_id = harness.add_relation(relation_name, other_app)
+        harness.add_relation_unit(rel_id, other_unit)
+        harness.update_relation_data(rel_id, other_app, relation_data)
+
+    with expected_raises as partial_relation_data:
+        interfaces = harness.charm._get_interfaces()
+        data = harness.charm._get_object_storage(interfaces)
+    if expected_status is None:
+        assert data == expected_returned_data
+    else:
+        assert partial_relation_data.value.status == expected_status
 
 
 @pytest.fixture
