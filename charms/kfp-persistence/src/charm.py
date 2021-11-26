e62fdc6b94e898f3434c7aa9c0233d18a6eaa792
Andrew Scribner <ca.scribner+1@gmail.com>
2021-11-26 11:34:23 -0500

    feat: refactor status handling for kfp-persistence

--- charms/kfp-persistence/src/charm.py
+++ charms/kfp-persistence/src/charm.py
@@ -2,6 +2,7 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+from jsonschema import ValidationError
 import logging
 
 from oci_image import OCIImageResource, OCIImageResourceError
@@ -9,6 +10,7 @@ from ops.charm import CharmBase
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
 from serialized_data_interface import (
+    SerializedDataInterface,
     NoCompatibleVersions,
     NoVersionsListed,
     get_interfaces,
@@ -17,53 +19,39 @@ from serialized_data_interface import (
 log = logging.getLogger()
 
 
-class Operator(CharmBase):
+class KfpPersistenceOperator(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
 
-        if not self.model.unit.is_leader():
-            log.info("Not a leader, skipping set_pod_spec")
-            self.model.unit.status = ActiveStatus()
-            return
-
-        try:
-            self.interfaces = get_interfaces(self)
-        except NoVersionsListed as err:
-            self.model.unit.status = WaitingStatus(str(err))
-            return
-        except NoCompatibleVersions as err:
-            self.model.unit.status = BlockedStatus(str(err))
-            return
-        else:
-            self.model.unit.status = ActiveStatus()
-
+        self.log = logging.getLogger()
         self.image = OCIImageResource(self, "oci-image")
-        self.framework.observe(self.on.install, self.set_pod_spec)
-        self.framework.observe(self.on.upgrade_charm, self.set_pod_spec)
-        self.framework.observe(self.on.config_changed, self.set_pod_spec)
+
+        self.framework.observe(self.on.install, self.main)
+        self.framework.observe(self.on.upgrade_charm, self.main)
+        self.framework.observe(self.on.config_changed, self.main)
         # TODO: Need to add a relation_changed hook here too, otherwise
         #  "Waiting for api relation data" wont resolve naturally
 
         for relation in self.interfaces.keys():
             self.framework.observe(
                 self.on[relation].relation_changed,
-                self.set_pod_spec,
+                self.main,
             )
 
-    def set_pod_spec(self, event):
+    def main(self, event):
+        # Set up all relations/fetch required data
         try:
+            self._check_leader()
+            interfaces = self._get_interfaces()
             image_details = self.image.fetch()
-        except OCIImageResourceError as e:
-            self.model.unit.status = e.status
-            log.info(e)
+            kfpapi = self._get_kfpapi(interfaces)
+        except (CheckFailed, OCIImageResourceError) as check_failed:
+            self.model.unit.status = check_failed.status
+            self.log.info(str(check_failed.status))
             return
-
-        if not ((api := self.interfaces["kfp-api"]) and api.get_data()):
-            self.model.unit.status = WaitingStatus("Waiting for api relation data")
-            return
-        api = list(api.get_data().values())[0]
 
         self.model.unit.status = MaintenanceStatus("Setting pod spec")
+
         self.model.pod.set_spec(
             {
                 "version": 3,
@@ -96,7 +84,7 @@ class Operator(CharmBase):
                             "--namespace=",
                             "--ttlSecondsAfterWorkflowFinish=86400",
                             "--numWorker=2",
-                            f"--mlPipelineAPIServerName={api['service-name']}",
+                            f"--mlPipelineAPIServerName={kfpapi['service-name']}",
                         ],
                     }
                 ],
@@ -104,6 +92,92 @@ class Operator(CharmBase):
         )
         self.model.unit.status = ActiveStatus()
 
+    def _check_leader(self):
+        if not self.unit.is_leader():
+            # We can't do anything useful when not the leader, so do nothing.
+            raise CheckFailed("Waiting for leadership", WaitingStatus)
+
+    def _get_interfaces(self):
+        # Remove this abstraction when SDI adds .status attribute to NoVersionsListed,
+        # NoCompatibleVersionsListed:
+        # https://github.com/canonical/serialized-data-interface/issues/26
+        try:
+            interfaces = get_interfaces(self)
+        except NoVersionsListed as err:
+            raise CheckFailed(str(err), WaitingStatus)
+        except NoCompatibleVersions as err:
+            raise CheckFailed(str(err), BlockedStatus)
+        return interfaces
+
+    def _get_object_storage(self, interfaces):
+        relation_name = "object-storage"
+        return self.validate_sdi_interface(interfaces, relation_name)
+
+    def validate_sdi_interface(self, interfaces: dict, relation_name: str, default_return=None):
+        """Validates data received from SerializedDataInterface, returning the data if valid
+
+        Optionally can return a default_return value when no relation is established
+
+        Raises:
+            CheckFailed(..., Blocked) when no relation established (unless default_return set)
+            CheckFailed(..., Blocked) if interface is not using SDI
+            CheckFailed(..., Blocked) if data in interface fails schema check
+            CheckFailed(..., Waiting) if we have a relation established but no data passed
+
+        Params:
+            interfaces:
+
+        Returns:
+              (dict) interface data
+        """
+        # If nothing is related to this relation, return a default value or raise an error
+        if relation_name not in interfaces or interfaces[relation_name] is None:
+            if default_return is not None:
+                return default_return
+            else:
+                raise CheckFailed(f"Missing required relation for {relation_name}", BlockedStatus)
+
+        relations = interfaces[relation_name]
+        if not isinstance(relations, SerializedDataInterface):
+            raise CheckFailed(f"Unexpected error with {relation_name} relation data - data not as expected", BlockedStatus)
+
+        # Get and validate data from the relation
+        try:
+            # relations is a dict of {(ops.model.Relation, ops.model.Application): data}
+            unpacked_relation_data = relations.get_data()
+        except ValidationError as val_error:
+            # Validation in .get_data() ensures if data is populated, it matches the schema and is
+            # not incomplete
+            self.log.error(val_error)
+            raise CheckFailed(
+                f"Found incomplete/incorrect relation data for {relation_name}.  See logs",
+                BlockedStatus
+            )
+
+        # Check if we have an established relation with no data exchanged
+        if len(unpacked_relation_data) == 0:
+            raise CheckFailed(f"Waiting for {relation_name} relation data", WaitingStatus)
+
+        # Unpack data (we care only about the first element)
+        data_dict = list(unpacked_relation_data.values())[0]
+
+        # Catch if empty data dict is received (JSONSchema ValidationError above does not raise
+        # when this happens)
+        if len(data_dict) == 0:
+            raise CheckFailed(f"Found incomplete/incorrect relation data for {relation_name}.", BlockedStatus)
+
+        return data_dict
+
+
+class CheckFailed(Exception):
+    """Raise this exception if one of the checks in main fails."""
+
+    def __init__(self, msg, status_type=None):
+        super().__init__()
+
+        self.msg = msg
+        self.status_type = status_type
+        self.status = status_type(msg)
 
 if __name__ == "__main__":
-    main(Operator)
+    main(KfpPersistenceOperator)
