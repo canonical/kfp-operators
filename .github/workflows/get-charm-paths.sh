dbaa8175109d1c5f4ebb22681cf9cf10ec951761
Andrew Scribner <ca.scribner+1@gmail.com>
2021-12-02 15:31:47 -0500

    fix: point charmhub-upload-action to branch/upload-charm-revisions
    for testing only

--- .github/workflows/get-charm-paths.sh
+++ .github/workflows/get-charm-paths.sh
@@ -25,4 +25,6 @@ fi
 # { charm_paths: [...] }
 CHARM_PATHS_JSON=$(echo "$CHARM_PATHS" | jq --slurp --raw-input 'split("\n")[:-1]' | jq "{\"charm_path\": . }" | jq -c)
 
+echo "Found CHARM_PATHS_JSON: $CHARM_PATHS_JSON"
+
 echo "::set-output name=CHARM_PATHS_JSON::$CHARM_PATHS_JSON"
