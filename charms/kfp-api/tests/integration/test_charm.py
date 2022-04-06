ff64387d01dd0b6d8a0f095691766950412ac95e
natalia <natalianowakowska@mail.com>
2022-04-06 12:26:47 +0200

    Updated integration test and relation name

--- charms/kfp-api/tests/integration/test_charm.py
+++ charms/kfp-api/tests/integration/test_charm.py
@@ -9,6 +9,7 @@ import pytest
 import requests
 import yaml
 from pytest_operator.plugin import OpsTest
+from tenacity import RetryError, Retrying, stop_after_attempt, stop_after_delay, wait_exponential
 
 logger = logging.getLogger(__name__)
 
@@ -69,7 +70,6 @@ async def test_prometheus_grafana_integration(ops_test: OpsTest):
     await ops_test.model.deploy(grafana, channel="latest/beta")
     await ops_test.model.add_relation(prometheus, grafana)
     await ops_test.model.add_relation(APP_NAME, grafana)
-    await ops_test.model.add_relation(prometheus, APP_NAME)
     await ops_test.model.deploy(
         prometheus_scrape_charm,
         channel="latest/beta",
@@ -83,16 +83,27 @@ async def test_prometheus_grafana_integration(ops_test: OpsTest):
     prometheus_unit_ip = status["applications"][prometheus]["units"][f"{prometheus}/0"]["address"]
     logger.info(f"Prometheus available at http://{prometheus_unit_ip}:9090")
 
-    r = requests.get(
-        f'http://{prometheus_unit_ip}:9090/api/v1/query?query=up{{juju_application="{APP_NAME}"}}'
-    )
-    response = json.loads(r.content.decode("utf-8"))
-    response_status = response["status"]
-    logger.info(f"Response status is {response_status}")
+    try:
+        for attempt in Retrying(
+                (stop_after_attempt(5) | stop_after_delay(30)),
+                wait=wait_exponential(multiplier=1, min=1, max=10),
+                reraise=True,
+        ):
+            with attempt:
+                r = requests.get(
+                    f'http://{prometheus_unit_ip}:9090/api/v1/query?'
+                    f'query=up{{juju_application="{APP_NAME}"}}'
+                )
+                response = json.loads(r.content.decode("utf-8"))
+                response_status = response["status"]
+                logger.info(f"Response status is {response_status}")
+                assert response_status == "success"
 
-    response_metric = response["data"]["result"][0]["metric"]
-    assert response_metric["juju_application"] == APP_NAME
-    assert response_metric["juju_model"] == ops_test.model_name
+                response_metric = response["data"]["result"][0]["metric"]
+                assert response_metric["juju_application"] == APP_NAME
+                assert response_metric["juju_model"] == ops_test.model_name
+    except RetryError:
+        logger.info("Testing prometheus failed")
 
 
 # TODO: Add test that makes sure the Grafana relation actually works
