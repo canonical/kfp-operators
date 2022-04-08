8a6114265eb0296c182f64d735cd5da1fcc2bb6e
natalia <natalianowakowska@mail.com>
2022-04-08 12:11:46 +0200

    Updated the retry func

--- charms/kfp-api/tests/integration/test_charm.py
+++ charms/kfp-api/tests/integration/test_charm.py
@@ -9,7 +9,7 @@ import pytest
 import requests
 import yaml
 from pytest_operator.plugin import OpsTest
-from tenacity import RetryError, Retrying, stop_after_attempt, stop_after_delay, wait_exponential
+from tenacity import Retrying, stop_after_attempt, stop_after_delay, wait_exponential
 
 logger = logging.getLogger(__name__)
 
@@ -83,28 +83,32 @@ async def test_prometheus_grafana_integration(ops_test: OpsTest):
     prometheus_unit_ip = status["applications"][prometheus]["units"][f"{prometheus}/0"]["address"]
     logger.info(f"Prometheus available at http://{prometheus_unit_ip}:9090")
 
-    try:
-        for attempt in Retrying(
-                (stop_after_attempt(5) | stop_after_delay(30)),
-                wait=wait_exponential(multiplier=1, min=1, max=10),
-                reraise=True,
-        ):
-            with attempt:
-                r = requests.get(
-                    f'http://{prometheus_unit_ip}:9090/api/v1/query?'
-                    f'query=up{{juju_application="{APP_NAME}"}}'
-                )
-                response = json.loads(r.content.decode("utf-8"))
-                response_status = response["status"]
-                logger.info(f"Response status is {response_status}")
-                assert response_status == "success"
+    for attempt in retry_for_5_attempts:
+        logger.info(
+            f"Testing prometheus deployment (attempt "
+            f"{attempt.retry_state.attempt_number})"
+        )
+        with attempt:
+            r = requests.get(
+                f'http://{prometheus_unit_ip}:9090/api/v1/query?'
+                f'query=up{{juju_application="{APP_NAME}"}}'
+            )
+            response = json.loads(r.content.decode("utf-8"))
+            response_status = response["status"]
+            logger.info(f"Response status is {response_status}")
+            assert response_status == "success"
 
-                response_metric = response["data"]["result"][0]["metric"]
-                assert response_metric["juju_application"] == APP_NAME
-                assert response_metric["juju_model"] == ops_test.model_name
-    except RetryError:
-        logger.info("Testing prometheus failed")
+            response_metric = response["data"]["result"][0]["metric"]
+            assert response_metric["juju_application"] == APP_NAME
+            assert response_metric["juju_model"] == ops_test.model_name
 
+
+# Helper to retry calling a function over 30 seconds or 5 attempts
+retry_for_5_attempts = Retrying(
+    stop=(stop_after_attempt(5) | stop_after_delay(30)),
+    wait=wait_exponential(multiplier=1, min=1, max=10),
+    reraise=True,
+)
 
 # TODO: Add test that makes sure the Grafana relation actually works
 #  once the template is defined.
