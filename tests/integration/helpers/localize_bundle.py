b1d425d073654b9666a05f39169df986669b303d
Andrew Scribner <ca.scribner+1@gmail.com>
2022-07-26 13:57:06 -0400

    feat: convert bundle tests to pytest-operator
    This initial commit adds a pytest-operator driven test suite for the bundle.  Previously, the bundle was deployed "manually" in integrate.yaml.  This refactors the test suite to deploy using pytest-operator and by substituting locally built charms into a template bundle.
    
    Updated tests are not run by CI, but can be executed locally through `tox -e bundle-integration`

---
+++ tests/integration/helpers/localize_bundle.py
@@ -0,0 +1,97 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+import copy
+from pathlib import Path
+from typing import Union, Optional
+import yaml
+from zipfile import ZipFile
+
+
+# TODO: Move this somewhere more general
+
+def get_charm_name(metadata_file: Union[Path, str]) -> str:
+    metadata = yaml.safe_load(Path(metadata_file).read_text())
+    return metadata["name"]
+
+
+def get_charm_file(charm_dir: Path) -> Path:
+    """Returns the path to the .charm file representing the charm in the given directory
+
+    TODO: This just assumes the suffix on the file name will be "ubuntu-20.04-amd64".
+          Fix this in future
+    """
+    charm_dir = Path(charm_dir)
+    metadata_file = charm_dir / "metadata.yaml"
+    charm_name = get_charm_name(metadata_file)
+
+    return (charm_dir / f"{charm_name}_ubuntu-20.04-amd64.charm").absolute()
+
+
+def get_resources_from_charm_dir(charm_dir: Path) -> dict[str:str]:
+    """Returns the resources of the charm at path"""
+    metadata_file = charm_dir / "metadata.yaml"
+    metadata = yaml.safe_load(Path(metadata_file).read_text())
+    resources = metadata["resources"]
+    return {k: v["upstream-source"] for k, v in resources.items()}
+
+
+def get_resources_from_charm_file(charm_file: str) -> dict[str, str]:
+    """Extracts the resources of a charm from a .charm (zipped) file."""
+    with ZipFile(charm_file, 'r') as zip:
+        metadata_file = zip.open('metadata.yaml')
+        metadata = yaml.safe_load(metadata_file)
+        resources = metadata["resources"]
+        return {k: v["upstream-source"] for k, v in resources.items()}
+    open_charm_file = charm_file
+
+
+def localize_bundle_application(
+    bundle: dict,
+    application: str,
+    charm_dir: Optional[Path] = None,
+    charm_file: Optional[Path] = None,
+    resources: Optional[dict] = None,
+):
+    """Localize an application in a bundle, replacing its charm and resource with local files
+
+    TODO: better docstring
+    charm_file and resources can optionally be provided, otherwise they will be inferred from
+    charm_dir.  If we provide charm_file and not resources, resources will be inferred from the
+    metadata.yaml file in the charm_file.
+    """
+    bundle = copy.deepcopy(bundle)
+
+    if not (charm_file or charm_dir):
+        raise ValueError("Either charm_file or charm_dir must be provided")
+
+    if charm_file:
+        if not resources:
+            resources = get_resources_from_charm_file(charm_file)
+    else:
+        charm_file = get_charm_file(charm_dir)
+        if not resources:
+            resources = get_resources_from_charm_dir(charm_dir)
+
+    bundle["applications"][application]["charm"] = str(charm_file)
+    bundle["applications"][application]["resources"] = resources
+    bundle["applications"][application]["_channel"] = bundle["applications"][application][
+        "channel"
+    ]
+    del bundle["applications"][application]["channel"]
+
+    return bundle
+
+
+def main(bundle_file: str, application: str, charm_dir: str, output_file: str):
+    bundle = yaml.safe_load(Path(bundle_file).read_text())
+    charm_dir = Path(charm_dir)
+    output_bundle = localize_bundle_application(bundle, application, charm_dir)
+
+    with open(output_file, "w") as fout:
+        yaml.dump(output_bundle, fout)
+
+
+if __name__ == "__main__":
+    import typer
+
+    typer.run(main)
---
+++ tests/integration/test_bundle.py
@@ -0,0 +1,96 @@
+import logging
+from pathlib import Path, PosixPath
+import shlex
+import yaml
+
+from pytest_operator.plugin import OpsTest
+from helpers.localize_bundle import localize_bundle_application
+
+BUNDLE_CHARMS = [
+    "kfp-api",
+    "kfp-persistence",
+    "kfp-profile-controller",
+    "kfp-schedwf",
+    "kfp-ui",
+    "kfp-viewer",
+    "kfp-viz",
+]
+
+CHARM_PATH_TEMPLATE = "{basedir}/charms/{charm}/"
+
+log = logging.getLogger(__name__)
+
+
+async def test_build_and_deploy(ops_test: OpsTest):
+    # for each local charm, build it (in parallel?) and update the bundle to use that
+    # TODO: Optionally override some local charms, pulling them instead?  And maybe that accepts a
+    #       path (to an already built charm) or channel (so we can use a published branch)
+    #       This would be good for CI, where we could build in parallel across many jobs then test
+    #       in one spot
+
+    # TODO: Should I make a generic "build charm if not already built" function that accepts a
+    #       bundle? it will basically just be `juju bundle`...
+
+    # TODO: Separate kfp charms and other dependencies into bundle and overlay, that way they can
+    #       be deployed in separate tests.  But do we need extra tooling to let us "deploy" a
+    #       overlay after the fact?  Can't just `juju deploy` it, can we?
+    #       Yeah actually, you'd deploy the dependencies first, then you could deploy this bundle's
+    #       charms as an overlay.  Ideally you'd build this bundle's charms first though, so you
+    #       can fail before.  If you're really worried about speed, you build, deploy these without
+    #       relations to dependencies, then deploy dependencies, then wire these up
+    #
+    # TODO: Update bundle first
+
+    if ops_test.model_name != "kubeflow":
+        raise ValueError("kfp must be deployed to namespace kubeflow")
+
+    basedir = Path("./").absolute()
+
+    # Load template bundle
+    bundlefile = basedir / "tests/integration/data/kfp_minimal.yaml"
+    bundle = yaml.safe_load(Path(bundlefile).read_text())
+
+    # Build the charms we need to build
+    charms_to_build = {
+        charm: Path(CHARM_PATH_TEMPLATE.format(basedir=str(basedir), charm=charm))
+        for charm in BUNDLE_CHARMS
+    }
+    log.info(f"Building charms for: {charms_to_build}")
+    # Mock out build_charms during iterations
+    built_charms = await ops_test.build_charms(*charms_to_build.values())
+    # FIXME: debug code
+    # built_charms = {
+    #     'kfp-api': PosixPath('/home/scribs/code/canonical/kfp-operators/kf-16-release/charms/kfp-api/kfp-api_ubuntu-20.04-amd64.charm'),
+    #     # 'kfp-viewer': PosixPath('/home/scribs/code/canonical/kfp-operators/kf-16-release/charms/kfp-viewer/kfp-viewer_ubuntu-20.04-amd64.charm'),
+    #     # 'kfp-viz': PosixPath('/home/scribs/code/canonical/kfp-operators/kf-16-release/charms/kfp-viz/kfp-viz_ubuntu-20.04-amd64.charm')
+    # }
+    log.info(f"Built charms: {built_charms}")
+
+    # Edit the bundle, localizing the charms we've built
+    for charm, path in built_charms.items():
+        bundle = localize_bundle_application(
+            bundle, application=charm, charm_file=path
+        )
+
+    # Deploy the bundle
+    # This should be easier... but it isn't.  Can either use the `juju bundle` integration, or
+    # shell out to juju (like k8s charms do).  For now, I'm doing the latter
+    bundles_dst_dir = ops_test.tmp_path / "bundles"
+    bundles_dst_dir.mkdir(exist_ok=True)
+    bundle_dst_file = bundles_dst_dir / "bundle.yaml"
+    bundle_dst_file.write_text(yaml.dump(bundle))
+
+    model = ops_test.model_full_name
+    # TODO: Figure out when we need --trust rather than blanket-applying it
+    cmd = f"juju deploy -m {model} --trust {bundle_dst_file}"
+    log.info(f"Deploying bundle to {model} using cmd '{cmd}'")
+    rc, stdout, stderr = await ops_test.run(*shlex.split(cmd))
+
+    # Wait for everything to be up.  Note, at time of writing these charms would naturally go
+    # into blocked during deploy while waiting for each other to satisfy relations.
+    await ops_test.model.wait_for_idle(
+        status="active",
+        raise_on_blocked=False,  # These apps block while waiting for each other to deploy/relate
+        raise_on_error=True,
+        timeout=1200,
+    )
