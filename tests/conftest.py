ebcd83792b04f341baa6b6aae42af045fd5c44b3
Andrew Scribner <ca.scribner+1@gmail.com>
2022-07-27 14:48:34 -0400

    feat: convert bundle tests to pytest-operator (#85)
    This adds a pytest-operator driven test suite for the bundle.  Previously, the bundle was deployed "manually" in integrate.yaml.  This refactors the test suite to deploy using pytest-operator and by substituting locally built charms into a template bundle.
    
    This commit includes some transferable helpers that should in future be migrated to a shared repo (either in pytest-operator or somewhere else like charmed-kubeflow-chisme)
    
    Also included: 
    * Adds notes to disabled test_pipelines.py

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+from _pytest.config.argparsing import Parser
+
+
+def pytest_addoption(parser: Parser):
+    parser.addoption(
+        "--bundle",
+        default="./tests/integration/data/kfp_against_latest_edge.yaml",
+        help="Path to bundle file to use as the template for tests.  This must include all charms"
+             "built by this bundle, where the locally built charms will replace those specified. "
+             "This is useful for testing this bundle against different external dependencies. "
+             "An example file is in ./tests/integration/data/kfp_against_latest_edge.yaml",
+    )
