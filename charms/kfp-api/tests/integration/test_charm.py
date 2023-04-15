58615a72a11fbac5fafad1796b04fcb1a7be1c31
i-chvets <113444075+i-chvets@users.noreply.github.com>
2023-04-15 15:24:36 -0400

    Kf 1668 gh188 feat kfp api sidecar rewrite (#193)
    https://github.com/canonical/kfp-operators/issues/188
    
    Summary of changes:
    - Updated tox.ini to run test properly.
    - Modified charm code to use sidecar pattern as per approved
      specification.
    - Added required manifests.
    - Modified existing tests to work with new charm.
    - Modified test bundle definitions and helpers.
    - Added requirements.
    
    NOTE: Additional integration and unit tests, upgrade and update status
    handlers, aggregation rules will be added in another PRs.
    
    - Added service patch lib needed by charm.
    - Added missing lib files.
    - Removed not needed exception handling.
    - Modified error handling and added log message for failed configuration
      generation.
    - Removed not needed default.
    - Modified unit tests accordingly.
    - Added mock to unit test.
    - Modified code to use pathlib.
    - Added model-config to set default series to focal.

--- charms/kfp-api/tests/integration/test_charm.py
+++ charms/kfp-api/tests/integration/test_charm.py
@@ -29,6 +29,9 @@ async def test_build_and_deploy(ops_test: OpsTest):
     image_path = METADATA["resources"]["oci-image"]["upstream-source"]
     resources = {"oci-image": image_path}
 
+    # set default series for test model
+    await ops_test.juju("model-config", "default-series=focal")
+
     await ops_test.model.deploy(
         entity_url=built_charm_path,
         application_name=APP_NAME,
@@ -37,26 +40,25 @@ async def test_build_and_deploy(ops_test: OpsTest):
     )
 
     await ops_test.model.deploy(
-        entity_url="charmed-osm-mariadb-k8s", application_name="kfp-db", config=KFP_DB_CONFIG
+        entity_url="charmed-osm-mariadb-k8s",
+        application_name="kfp-db",
+        config=KFP_DB_CONFIG,
+        channel="latest/stable",
+        trust=True,
     )
-    await ops_test.model.add_relation(
-        f"{APP_NAME}:mysql",
-        "kfp-db:mysql",
-    )
+    await ops_test.model.add_relation(f"{APP_NAME}:mysql", "kfp-db:mysql")
 
-    await ops_test.model.deploy(entity_url="minio", config=MINIO_CONFIG)
-    await ops_test.model.add_relation(
-        f"{APP_NAME}:object-storage",
-        "minio:object-storage",
+    await ops_test.model.deploy(
+        entity_url="minio", config=MINIO_CONFIG, channel="ckf-1.7/stable", trust=True
     )
+    await ops_test.model.add_relation(f"{APP_NAME}:object-storage", "minio:object-storage")
 
-    await ops_test.model.deploy(entity_url="kfp-viz")
-    await ops_test.model.add_relation(
-        f"{APP_NAME}:kfp-viz",
-        "kfp-viz:kfp-viz",
+    await ops_test.model.deploy(entity_url="kfp-viz", channel="2.0/stable", trust=True)
+    await ops_test.model.add_relation(f"{APP_NAME}:kfp-viz", "kfp-viz:kfp-viz")
+
+    await ops_test.model.wait_for_idle(
+        status="active", raise_on_blocked=False, raise_on_error=False, timeout=60 * 10
     )
-
-    await ops_test.model.wait_for_idle(status="active", timeout=60 * 10)
 
 
 async def test_prometheus_grafana_integration(ops_test: OpsTest):
@@ -67,9 +69,11 @@ async def test_prometheus_grafana_integration(ops_test: OpsTest):
     scrape_config = {"scrape_interval": "30s"}
 
     # Deploy and relate prometheus
-    await ops_test.model.deploy(prometheus, channel="latest/edge", trust=True)
-    await ops_test.model.deploy(grafana, channel="latest/edge", trust=True)
-    await ops_test.model.deploy(prometheus_scrape, channel="latest/stable", config=scrape_config)
+    await ops_test.model.deploy(prometheus, channel="latest/stable", trust=True)
+    await ops_test.model.deploy(grafana, channel="latest/stable", trust=True)
+    await ops_test.model.deploy(
+        prometheus_scrape, channel="latest/stable", config=scrape_config, trust=True
+    )
 
     await ops_test.model.add_relation(APP_NAME, prometheus_scrape)
     await ops_test.model.add_relation(
@@ -82,7 +86,9 @@ async def test_prometheus_grafana_integration(ops_test: OpsTest):
         f"{prometheus}:metrics-endpoint", f"{prometheus_scrape}:metrics-endpoint"
     )
 
-    await ops_test.model.wait_for_idle(status="active", timeout=90 * 20)
+    await ops_test.model.wait_for_idle(
+        status="active", raise_on_blocked=False, raise_on_error=False, timeout=60 * 20
+    )
 
     status = await ops_test.model.get_status()
     prometheus_unit_ip = status["applications"][prometheus]["units"][f"{prometheus}/0"]["address"]
