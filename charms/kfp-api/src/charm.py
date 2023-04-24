b2de35f72336bfce6e6c06d6c29658e6beed751d
Ivan Chvets <ivan.chvets@canonical.com>
2023-04-24 19:18:54 -0400

    fix: added on_event call
    Summary of changes:
    - Added call to main event handler to unblocked the charm.

--- charms/kfp-api/src/charm.py
+++ charms/kfp-api/src/charm.py
@@ -587,12 +587,12 @@ class KfpApiOperator(CharmBase):
 
     def _on_update_status(self, _):
         """Update status actions."""
-        # skip update status processing in case of BlockedStatus or WaitingStatus
+        # execute main eevent handler case of BlockedStatus or WaitingStatus
         # WaitingStatus assumes DB relation is empty/incorrect which prevents workload from
         # starting
         status = self.model.unit.status
         if isinstance(status, BlockedStatus) or isinstance(status, WaitingStatus):
-            return
+            self._on_event(_)
         try:
             self._check_status()
         except ErrorWithStatus as err:
