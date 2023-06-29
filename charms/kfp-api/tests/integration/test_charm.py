635d1092363dd2f01b3fbd9e6726455d6697d342
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-06-29 19:21:03 +0200

    [skip] modify individual tests

--- charms/kfp-api/tests/integration/test_charm.py
+++ charms/kfp-api/tests/integration/test_charm.py
@@ -3,6 +3,7 @@
 
 import json
 import logging
+import os
 from pathlib import Path
 
 import pytest
@@ -18,6 +19,24 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 
 MINIO_CONFIG = {"access-key": "minio", "secret-key": "minio-secret-key"}
 KFP_DB_CONFIG = {"database": "mlpipeline"}
+
+@pytest.fixture
+def ops_test(ops_test: OpsTest) -> OpsTest:
+    if os.environ.get("CI") == "true":
+        # Running in GitHub Actions; skip build step
+        # (GitHub Actions uses a separate, cached build step)
+        packed_charms = json.loads(os.environ["CI_PACKED_CHARMS"])
+
+        async def build_charm(charm_path, bases_index: int = None) -> Path:
+            for charm in packed_charms:
+                if Path(charm_path) == Path(charm["directory_path"]):
+                    if bases_index is None or bases_index == charm["bases_index"]:
+                        return charm["file_path"]
+            raise ValueError(f"Unable to find .charm file for {bases_index=} at {charm_path=}")
+
+        ops_test.build_charm = build_charm
+
+    return ops_test
 
 
 class TestCharm:
--- charms/kfp-profile-controller/tests/integration/test_charm.py
+++ charms/kfp-profile-controller/tests/integration/test_charm.py
@@ -1,7 +1,9 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import json
 import logging
+import os
 from base64 import b64decode
 from pathlib import Path
 
@@ -21,6 +23,24 @@ METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 
 MINIO_APP_NAME = "minio"
 MINIO_CONFIG = {"access-key": "minio", "secret-key": "minio-secret-key"}
+
+@pytest.fixture
+def ops_test(ops_test: OpsTest) -> OpsTest:
+    if os.environ.get("CI") == "true":
+        # Running in GitHub Actions; skip build step
+        # (GitHub Actions uses a separate, cached build step)
+        packed_charms = json.loads(os.environ["CI_PACKED_CHARMS"])
+
+        async def build_charm(charm_path, bases_index: int = None) -> Path:
+            for charm in packed_charms:
+                if Path(charm_path) == Path(charm["directory_path"]):
+                    if bases_index is None or bases_index == charm["bases_index"]:
+                        return charm["file_path"]
+            raise ValueError(f"Unable to find .charm file for {bases_index=} at {charm_path=}")
+
+        ops_test.build_charm = build_charm
+
+    return ops_test
 
 
 @pytest.mark.abort_on_fail
