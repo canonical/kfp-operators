601af26f0c6b350f5f770f30a86a5305640479f5
Ivan Chvets <ivan.chvets@canonical.com>
2023-07-26 19:55:04 -0400

    fix: increased timeout in bundle testing

--- tests/integration/test_kfp_functional.py
+++ tests/integration/test_kfp_functional.py
@@ -73,7 +73,7 @@ async def test_build_and_deploy(ops_test: OpsTest, request, lightkube_client):
         status="active",
         raise_on_blocked=False,  # These apps block while waiting for each other to deploy/relate
         raise_on_error=True,
-        timeout=3600,
+        timeout=4800,
         idle_period=120,
     )
 
