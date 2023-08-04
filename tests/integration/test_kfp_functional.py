f002da76547b99734b05f7d7bc6cb985887ab7e9
Andrew Scribner <ca.scribner+1@gmail.com>
2023-08-04 09:22:49 -0400

    debug: disable non-build integration tests

--- tests/integration/test_kfp_functional.py
+++ tests/integration/test_kfp_functional.py
@@ -78,118 +78,118 @@ async def test_build_and_deploy(ops_test: OpsTest, request, lightkube_client):
     )
 
 # ---- KFP API Server focused test cases
-async def test_upload_pipeline(kfp_client):
-    """Upload a pipeline from a YAML file and assert its presence."""
-    # Upload a pipeline and get the server response
-    pipeline_upload_response = kfp_client.pipeline_uploads.upload_pipeline(
-        uploadfile=SAMPLE_PIPELINE, name="test-upload-pipeline"
-    )
-    # Upload a pipeline and get its ID
-    uploaded_pipeline_id = pipeline_upload_response.id
-
-    # Get pipeline id by name, default='sample-pipeline'
-    server_pipeline_id = kfp_client.get_pipeline_id(name="test-upload-pipeline")
-    assert uploaded_pipeline_id == server_pipeline_id
-
-async def test_create_and_monitor_run(kfp_client, create_and_clean_experiment):
-    """Create a run and monitor it to completion."""
-    # Create a run, save response in variable for easy manipulation
-    # Create an experiment for this run
-    experiment_response = create_and_clean_experiment
-
-    # Create a run from a pipeline file (SAMPLE_PIPELINE) and an experiment (create_experiment).
-    # This call uses the 'default' kubeflow service account to be able to edit Workflows
-    create_run_response = kfp_client.create_run_from_pipeline_package(
-        pipeline_file=SAMPLE_PIPELINE,
-        arguments={},
-        run_name="test-run-1",
-        experiment_name=experiment_response.name,
-        namespace=KUBEFLOW_PROFILE_NAMESPACE,
-    )
-
-    # FIXME: waiting_for_run_completion timeouts on GitHub runners
-    # Related issue: https://github.com/canonical/kfp-operators/issues/244
-    # Monitor the run to completion, the pipeline should not be executed in
-    # more than 300 seconds as it is a very simple operation
-    #monitor_response = kfp_client.wait_for_run_completion(create_run_response.run_id, timeout=600)
-
-    #assert monitor_response.run.status == "Succeeded"
-
-    # At least get the run and extract some data while the previous check
-    # works properly on the GitHub runners
-    test_run = kfp_client.get_run(create_run_response.run_id).run
-    assert test_run is not None
-
-# ---- ScheduledWorfklows and Argo focused test case
-async def test_create_and_monitor_recurring_run(
-    kfp_client, upload_and_clean_pipeline, create_and_clean_experiment
-):
-    """Create a recurring run and monitor it to completion."""
-
-    # Upload a pipeline from file
-    pipeline_response = upload_and_clean_pipeline
-
-    # Create an experiment for this run
-    experiment_response = create_and_clean_experiment
-
-    # Create a recurring run from a pipeline (upload_pipeline_from_file) and an experiment (create_experiment)
-    # This call uses the 'default' kubeflow service account to be able to edit ScheduledWorkflows
-    # This ScheduledWorkflow (Recurring Run) will run once every two seconds
-    create_recurring_run_response = kfp_client.create_recurring_run(
-        experiment_id=experiment_response.id,
-        job_name="recurring-job-1",
-        pipeline_id=pipeline_response.id,
-        enabled=True,
-        cron_expression="*/2 * * * * *",
-        max_concurrency=1,
-    )
-
-    recurring_job = create_recurring_run_response
-    # Assert the job is enabled
-    assert recurring_job.enabled is True
-
-    # Assert the job executes once every two seconds
-    assert recurring_job.trigger.cron_schedule.cron == "*/2 * * * * *"
-
-    # Wait for the recurring job to schedule some runs
-    time.sleep(6)
-
-    # FIXME: disabling the job does not work at the moment, it seems like
-    # the status of the recurring run is never updated and is causing the
-    # following assertion to fail
-    # Related issue: https://github.com/canonical/kfp-operators/issues/244
-    # Disable the job after few runs
-    kfp_client.disable_job(recurring_job.id)
-
-    # Assert the job is disabled
-    # assert recurring_job.enabled is False
-
-
-# ---- KFP Viewer and Visualization focused test cases
-async def test_apply_sample_viewer(lightkube_client):
-    """Test a Viewer can be applied and its presence is verified."""
-    # Create a Viewer namespaced resource
-    viewer_class_resource = create_namespaced_resource(
-        group="kubeflow.org", version="v1beta1", kind="Viewer", plural="viewers"
-    )
-
-    # Apply viewer
-    viewer_object = apply_manifests(lightkube_client, yaml_file_path=SAMPLE_VIEWER)
-
-    viewer = lightkube_client.get(
-        res=viewer_class_resource,
-        name=viewer_object.metadata.name,
-        namespace=viewer_object.metadata.namespace,
-    )
-    assert viewer is not None
-
-
-async def test_viz_server_healthcheck(ops_test: OpsTest):
-    """Run a healthcheck on the server endpoint."""
-    status = await ops_test.model.get_status()
-    units = status["applications"]["kfp-viz"]["units"]
-    url = units["kfp-viz/0"]["address"]
-    headers = {"kubeflow-userid": "user"}
-    result_status, result_text = await fetch_response(url=f"http://{url}:8888", headers=headers)
-
-    assert result_status == 200
+# async def test_upload_pipeline(kfp_client):
+#     """Upload a pipeline from a YAML file and assert its presence."""
+#     # Upload a pipeline and get the server response
+#     pipeline_upload_response = kfp_client.pipeline_uploads.upload_pipeline(
+#         uploadfile=SAMPLE_PIPELINE, name="test-upload-pipeline"
+#     )
+#     # Upload a pipeline and get its ID
+#     uploaded_pipeline_id = pipeline_upload_response.id
+#
+#     # Get pipeline id by name, default='sample-pipeline'
+#     server_pipeline_id = kfp_client.get_pipeline_id(name="test-upload-pipeline")
+#     assert uploaded_pipeline_id == server_pipeline_id
+#
+# async def test_create_and_monitor_run(kfp_client, create_and_clean_experiment):
+#     """Create a run and monitor it to completion."""
+#     # Create a run, save response in variable for easy manipulation
+#     # Create an experiment for this run
+#     experiment_response = create_and_clean_experiment
+#
+#     # Create a run from a pipeline file (SAMPLE_PIPELINE) and an experiment (create_experiment).
+#     # This call uses the 'default' kubeflow service account to be able to edit Workflows
+#     create_run_response = kfp_client.create_run_from_pipeline_package(
+#         pipeline_file=SAMPLE_PIPELINE,
+#         arguments={},
+#         run_name="test-run-1",
+#         experiment_name=experiment_response.name,
+#         namespace=KUBEFLOW_PROFILE_NAMESPACE,
+#     )
+#
+#     # FIXME: waiting_for_run_completion timeouts on GitHub runners
+#     # Related issue: https://github.com/canonical/kfp-operators/issues/244
+#     # Monitor the run to completion, the pipeline should not be executed in
+#     # more than 300 seconds as it is a very simple operation
+#     #monitor_response = kfp_client.wait_for_run_completion(create_run_response.run_id, timeout=600)
+#
+#     #assert monitor_response.run.status == "Succeeded"
+#
+#     # At least get the run and extract some data while the previous check
+#     # works properly on the GitHub runners
+#     test_run = kfp_client.get_run(create_run_response.run_id).run
+#     assert test_run is not None
+#
+# # ---- ScheduledWorfklows and Argo focused test case
+# async def test_create_and_monitor_recurring_run(
+#     kfp_client, upload_and_clean_pipeline, create_and_clean_experiment
+# ):
+#     """Create a recurring run and monitor it to completion."""
+#
+#     # Upload a pipeline from file
+#     pipeline_response = upload_and_clean_pipeline
+#
+#     # Create an experiment for this run
+#     experiment_response = create_and_clean_experiment
+#
+#     # Create a recurring run from a pipeline (upload_pipeline_from_file) and an experiment (create_experiment)
+#     # This call uses the 'default' kubeflow service account to be able to edit ScheduledWorkflows
+#     # This ScheduledWorkflow (Recurring Run) will run once every two seconds
+#     create_recurring_run_response = kfp_client.create_recurring_run(
+#         experiment_id=experiment_response.id,
+#         job_name="recurring-job-1",
+#         pipeline_id=pipeline_response.id,
+#         enabled=True,
+#         cron_expression="*/2 * * * * *",
+#         max_concurrency=1,
+#     )
+#
+#     recurring_job = create_recurring_run_response
+#     # Assert the job is enabled
+#     assert recurring_job.enabled is True
+#
+#     # Assert the job executes once every two seconds
+#     assert recurring_job.trigger.cron_schedule.cron == "*/2 * * * * *"
+#
+#     # Wait for the recurring job to schedule some runs
+#     time.sleep(6)
+#
+#     # FIXME: disabling the job does not work at the moment, it seems like
+#     # the status of the recurring run is never updated and is causing the
+#     # following assertion to fail
+#     # Related issue: https://github.com/canonical/kfp-operators/issues/244
+#     # Disable the job after few runs
+#     kfp_client.disable_job(recurring_job.id)
+#
+#     # Assert the job is disabled
+#     # assert recurring_job.enabled is False
+#
+#
+# # ---- KFP Viewer and Visualization focused test cases
+# async def test_apply_sample_viewer(lightkube_client):
+#     """Test a Viewer can be applied and its presence is verified."""
+#     # Create a Viewer namespaced resource
+#     viewer_class_resource = create_namespaced_resource(
+#         group="kubeflow.org", version="v1beta1", kind="Viewer", plural="viewers"
+#     )
+#
+#     # Apply viewer
+#     viewer_object = apply_manifests(lightkube_client, yaml_file_path=SAMPLE_VIEWER)
+#
+#     viewer = lightkube_client.get(
+#         res=viewer_class_resource,
+#         name=viewer_object.metadata.name,
+#         namespace=viewer_object.metadata.namespace,
+#     )
+#     assert viewer is not None
+#
+#
+# async def test_viz_server_healthcheck(ops_test: OpsTest):
+#     """Run a healthcheck on the server endpoint."""
+#     status = await ops_test.model.get_status()
+#     units = status["applications"]["kfp-viz"]["units"]
+#     url = units["kfp-viz/0"]["address"]
+#     headers = {"kubeflow-userid": "user"}
+#     result_status, result_text = await fetch_response(url=f"http://{url}:8888", headers=headers)
+#
+#     assert result_status == 200
