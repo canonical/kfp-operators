523a5611d1186bdca0c3cc42cd52c726fc9cb56c
Phoevos Kalemkeris <phoevos.kalemkeris@canonical.com>
2023-09-14 10:50:15 +0300

    Set session to scope for lightkube mock fixture
    Co-authored-by: Daniela Plascencia <daniela.plascencia@canonical.com>

--- charms/kfp-persistence/tests/unit/test_operator.py
+++ charms/kfp-persistence/tests/unit/test_operator.py
@@ -21,7 +21,7 @@ def harness():
     return harness
 
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def mocked_lightkube_client(mocker):
     """Mocks the Lightkube Client in charm.py, returning a mock instead."""
     mocked_lightkube_client = MagicMock()
