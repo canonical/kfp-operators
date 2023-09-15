d9d31f64e60176ff5ff25f09d235efd3e8e2e5c0
Ivan Chvets <ivan.chvets@canonical.com>
2023-09-15 16:06:55 -0400

    fix: updated requirements

--- tests/integration/test_bundle.py
+++ tests/integration/test_bundle.py
@@ -40,8 +40,8 @@ async def test_build_and_deploy(ops_test: OpsTest, request):
     log.info(f"Built charms: {built_charms}")
 
     # Edit the bundle, splicing our locally built charms into it
-    #for charm, path in built_charms.items():
-    #    bundle = localize_bundle_application(bundle, application=charm, charm_file=path)
+    for charm, path in built_charms.items():
+        bundle = localize_bundle_application(bundle, application=charm, charm_file=path)
 
     # Deploy the bundle
     # TODO This should be easier... but it isn't.  Can either use the `juju bundle` integration, or
@@ -57,44 +57,6 @@ async def test_build_and_deploy(ops_test: OpsTest, request):
     if rc != 0:
         raise RuntimeError(f"Failed to deploy bundle.  Got stdout:\n{stderr}\nand stderr:\n{stderr}")
 
-    # deploy argo-controller, minio separately, because those are PodSpec
-    # charms and cannot be deployed as part of a bundle
-    # for details refer to https://github.com/canonical/bundle-kubeflow/issues/693
-    await ops_test.model.deploy("argo-controller", channel="3.3/stable", trust=True)
-    await ops_test.model.deploy("minio", channel="ckf-1.7/stable", trust=True)
-    await ops_test.model.deploy(
-            "charmed-osm-mariadb-k8s", application_name="kfp-db", channel="latest/stable", trust=True
-    
-    )
-    await ops_test.model.relate("argo-controller", "minio")
-
-    await ops_test.model.wait_for_idle(
-        apps=["argo-controller", "minio", "kfp-db"],
-        status="active",
-        raise_on_blocked=False,  # These apps block while waiting for each other to deploy/relate
-        raise_on_error=True,
-        timeout=2400
-    )
-
-    # deploy all KFP charms that were built locally
-    for charm, path in built_charms.items():
-        metadata_file = Path(f"./charms/{charm}/metadata.yaml")
-        metadata = yaml.safe_load(metadata_file.read_text())
-        image_path = metadata["resources"]["oci-image"]["upstream-source"]
-        resources = {"oci-image": image_path}
-        await ops_test.model.deploy(charm, resources=resources, trust=True)
-
-    # add relations
-    await ops_test.model.relate("kfp-api", "kfp-db")
-    await ops_test.model.relate("kfp-api:kfp-api", "kfp-persistence:kfp-api")
-    await ops_test.model.relate("kfp-api:kfp-api", "kfp-ui:kfp-api")
-    await ops_test.model.relate("kfp-api:kfp-viz", "kfp-viz:kfp-viz")
-    await ops_test.model.relate("kfp-api:object-storage", "minio:object-storage")
-    await ops_test.model.relate("istio-pilot:ingress", "kfp-ui:ingress")
-    await ops_test.model.relate("kfp-api", "minio")
-    await ops_test.model.relate("kfp-profile-controller", "minio")
-    await ops_test.model.relate("kfp-ui", "minio")
-
     # Wait for everything to be up.  Note, at time of writing these charms would naturally go
     # into blocked during deploy while waiting for each other to satisfy relations, so we don't
     # raise_on_blocked.
@@ -102,7 +64,7 @@ async def test_build_and_deploy(ops_test: OpsTest, request):
         status="active",
         raise_on_blocked=False,  # These apps block while waiting for each other to deploy/relate
         raise_on_error=True,
-        timeout=2400,
+        timeout=1800,
     )
 
 
