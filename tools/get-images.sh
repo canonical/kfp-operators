8b5c5df8ac8f4e49a6817cb681756e4b8e42cc57
Ivan Chvets <ivan.chvets@canonical.com>
2023-09-15 09:47:29 -0400

    debug: added comment to trigger pr workflow

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -2,6 +2,7 @@
 #
 # This script returns list of container images that are managed by this charm and/or its workload
 #
+# DEBUGGING KFP integration tests: comment to trigger pull request workflow
 # dynamic list
 IMAGE_LIST=()
 IMAGE_LIST+=($(find -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
