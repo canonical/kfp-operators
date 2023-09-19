f58d34f858d474bee2b74b8a257a5ce4513d1798
Ivan Chvets <ivan.chvets@canonical.com>
2023-09-19 13:46:42 -0400

    feat: port kfp rocks to 1.7
    Summary of changes:
    - Updated metadata.yaml files for all charms to refer to published ROCK images.
    - Updated requirements.

--- tools/get-images.sh
+++ tools/get-images.sh
@@ -2,7 +2,6 @@
 #
 # This script returns list of container images that are managed by this charm and/or its workload
 #
-# DEBUGGING KFP integration tests: comment to trigger pull request workflow
 # dynamic list
 IMAGE_LIST=()
 IMAGE_LIST+=($(find -type f -name metadata.yaml -exec yq '.resources | to_entries | .[] | .value | ."upstream-source"' {} \;))
