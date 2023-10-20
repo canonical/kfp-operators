3b2bf14f752122b0ac8e286e47993912430fb37b
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2023-10-20 14:01:08 +0300

    feat: Add kfp-metadata-writer charm (#350)
    * feat: Introduce charm for KFP V2 metadata-writer component 
    * gitignore: Add venv folder
    * ci: Integrate kfp-metadata-reviewer charm
    * tests(bundle-integration): Reduce idle_period to 30 from 120 to 
    avoid huge amount of waiting and tests failing

---
+++ charms/kfp-metadata-writer/src/charm.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Charm for the Kubeflow Pipelines Metadata Writer.
+
+https://github.com/canonical/kfp-operators
+"""
+
+import logging
+
+import lightkube
+from charmed_kubeflow_chisme.components.charm_reconciler import CharmReconciler
+from charmed_kubeflow_chisme.components.kubernetes_component import KubernetesComponent
+from charmed_kubeflow_chisme.components.leadership_gate_component import LeadershipGateComponent
+from charmed_kubeflow_chisme.components.serialised_data_interface_components import (
+    SdiRelationDataReceiverComponent,
+)
+from charmed_kubeflow_chisme.kubernetes import create_charm_default_labels
+from lightkube.resources.core_v1 import ServiceAccount
+from lightkube.resources.rbac_authorization_v1 import ClusterRole, ClusterRoleBinding
+from ops.charm import CharmBase
+from ops.main import main
+
+from components.pebble_component import KfpMetadataWriterInputs, KfpMetadataWriterPebbleService
+
+logger = logging.getLogger(__name__)
+
+K8S_RESOURCE_FILES = ["src/templates/auth_manifests.yaml.j2"]
+
+
+class KfpMetadataWriter(CharmBase):
+    def __init__(self, *args):
+        """Charm for the Kubeflow Pipelines Metadata Writer."""
+        super().__init__(*args)
+
+        self.charm_reconciler = CharmReconciler(self)
+        self._namespace = self.model.name
+
+        self.leadership_gate = self.charm_reconciler.add(
+            component=LeadershipGateComponent(
+                charm=self,
+                name="leadership-gate",
+            ),
+            depends_on=[],
+        )
+
+        self.grpc_relation = self.charm_reconciler.add(
+            component=SdiRelationDataReceiverComponent(
+                charm=self, name="relation:grpc", relation_name="grpc"
+            ),
+            depends_on=[self.leadership_gate],
+        )
+
+        self.kubernetes_resources = self.charm_reconciler.add(
+            component=KubernetesComponent(
+                charm=self,
+                name="kubernetes:auth",
+                resource_templates=K8S_RESOURCE_FILES,
+                krh_resource_types={ClusterRole, ClusterRoleBinding, ServiceAccount},
+                krh_labels=create_charm_default_labels(
+                    self.app.name, self.model.name, scope="auth"
+                ),
+                context_callable=lambda: {"app_name": self.app.name, "namespace": self._namespace},
+                lightkube_client=lightkube.Client(),
+            ),
+            depends_on=[self.leadership_gate],
+        )
+
+        self.pebble_service_container = self.charm_reconciler.add(
+            component=KfpMetadataWriterPebbleService(
+                charm=self,
+                name="kfp-metadata-writer-pebble-service",
+                container_name="kfp-metadata-writer",
+                service_name="kfp-metadata-writer",
+                namespace_to_watch="",
+                inputs_getter=lambda: KfpMetadataWriterInputs(
+                    METADATA_GRPC_SERVICE_SERVICE_HOST=self.grpc_relation.component.get_data()[
+                        "service"
+                    ],
+                    METADATA_GRPC_SERVICE_SERVICE_PORT=self.grpc_relation.component.get_data()[
+                        "port"
+                    ],
+                ),
+            ),
+            depends_on=[self.kubernetes_resources, self.grpc_relation],
+        )
+
+        self.charm_reconciler.install_default_event_handlers()
+
+
+if __name__ == "__main__":
+    main(KfpMetadataWriter)
---
+++ charms/kfp-metadata-writer/src/components/pebble_component.py
@@ -0,0 +1,68 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+import dataclasses
+import logging
+
+from charmed_kubeflow_chisme.components.pebble_component import PebbleServiceComponent
+from ops.pebble import Layer
+
+logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class KfpMetadataWriterInputs:
+    """Defines the required inputs for KfpMetadataWriterInputs."""
+
+    METADATA_GRPC_SERVICE_SERVICE_HOST: str
+    METADATA_GRPC_SERVICE_SERVICE_PORT: str
+
+
+class KfpMetadataWriterPebbleService(PebbleServiceComponent):
+    def __init__(
+        self,
+        *args,
+        namespace_to_watch: str = "",
+        **kwargs,
+    ):
+        """Pebble service container component in order to configure Pebble layer
+
+        Args:
+            namespace_to_watch (str): The namespace in which the metadata-writer workload
+            will look for pods. Matching upstream manifests, this defaults to "" in order
+            for the component to look for pods in all namespaces.
+        """
+        self.namespace_to_watch = namespace_to_watch
+        super().__init__(*args, **kwargs)
+
+    def get_layer(self) -> Layer:
+        """Defines and returns Pebble layer configuration
+
+        This method is required for subclassing PebbleServiceContainer
+        """
+        logger.info("PebbleServiceComponent.get_layer executing")
+
+        try:
+            inputs: KfpMetadataWriterInputs = self._inputs_getter()
+        except Exception as err:
+            raise ValueError("Failed to get inputs for Pebble container.") from err
+
+        environment = {
+            "NAMESPACE_TO_WATCH": self.namespace_to_watch,
+            "METADATA_GRPC_SERVICE_SERVICE_HOST": inputs.METADATA_GRPC_SERVICE_SERVICE_HOST,
+            "METADATA_GRPC_SERVICE_SERVICE_PORT": inputs.METADATA_GRPC_SERVICE_SERVICE_PORT,
+        }
+        return Layer(
+            {
+                "summary": "kfp-metadata-writer layer",
+                "description": "Pebble config layer for kfp-metadata-writer",
+                "services": {
+                    self.service_name: {
+                        "override": "replace",
+                        "summary": "Entry point for kfp-metadata-writer oci-image",
+                        "command": "python3 -u /kfp/metadata_writer/metadata_writer.py",
+                        "startup": "enabled",
+                        "environment": environment,
+                    }
+                },
+            }
+        )
