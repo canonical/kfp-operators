0ccf8b30bfd92f28553fbf963066ff7b264a8950
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2023-10-20 12:16:56 +0300

    tests(unit): Remove redundant test
    Remove test_pebble_service_is_replanned_on_config_changed since charm
    has no configuration options.

--- charms/kfp-metadata-writer/tests/unit/test_operator.py
+++ charms/kfp-metadata-writer/tests/unit/test_operator.py
@@ -122,37 +122,6 @@ def test_pebble_service_container_running(harness, mocked_lightkube_client):
     )
 
 
-def test_pebble_service_is_replanned_on_config_changed(harness, mocked_lightkube_client):
-    """Test that the pebble service of the charm's kfp-metadata-writer container
-    is running on config_changed"""
-    harness.set_leader(True)
-    harness.begin()
-    harness.set_can_connect("kfp-metadata-writer", True)
-
-    harness.charm.kubernetes_resources.get_status = MagicMock(return_value=ActiveStatus())
-    add_sdi_relation_to_harness(harness, "grpc", data=MOCK_GRPC_DATA)
-
-    harness.charm.on.config_changed.emit()
-
-    assert isinstance(harness.charm.unit.status, ActiveStatus)
-
-    container = harness.charm.unit.get_container("kfp-metadata-writer")
-    # Assert that sidecar container is up and its service is running
-    assert container.get_service("kfp-metadata-writer").is_running()
-
-    # Assert the environment variables that are set from defaults in the service plan
-    environment = container.get_plan().services["kfp-metadata-writer"].environment
-    assert environment["NAMESPACE_TO_WATCH"] == ""
-    assert (
-        environment["METADATA_GRPC_SERVICE_SERVICE_HOST"]
-        == harness.charm.grpc_relation.component.get_data()["service"]
-    )
-    assert (
-        environment["METADATA_GRPC_SERVICE_SERVICE_PORT"]
-        == harness.charm.grpc_relation.component.get_data()["port"]
-    )
-
-
 def test_install_before_pebble_service_container(harness, mocked_lightkube_client):
     """Test that charm waits when install event happens before pebble-service-container is ready."""
     harness.set_leader(True)
