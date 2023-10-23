c10dfde38afa30e4bd8d1fdb1832d2b814ae194d
Daniela Plascencia <daniela.plascencia@canonical.com>
2023-10-23 13:01:17 +0200

    skip: add back ML_PIPELINE_VISUALIZATIONSERVER_SERVICE*

--- charms/kfp-api/src/charm.py
+++ charms/kfp-api/src/charm.py
@@ -258,6 +258,9 @@ class KfpApiOperator(CharmBase):
             "MULTIUSER": "true",
             "VISUALIZATIONSERVICE_NAME": viz_data["service-name"],
             "VISUALIZATIONSERVICE_PORT": viz_data["service-port"],
+            "ML_PIPELINE_VISUALIZATIONSERVER_SERVICE_HOST": viz_data["service-name"],
+            "ML_PIPELINE_VISUALIZATIONSERVER_SERVICE_PORT": viz_data["service-port"],
+            "CACHE_IMAGE": self.model.config["cache-image"],
         }
 
         return env_vars
