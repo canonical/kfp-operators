74be423d4ee11ef0703411344db9c0b1ff0d5426
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-01-18 11:20:20 +0200

    Use mysql-k8s 8.0/edge

--- charms/kfp-api/tests/integration/test_charm.py
+++ charms/kfp-api/tests/integration/test_charm.py
@@ -77,9 +77,11 @@ class TestCharm:
     async def test_relational_db_relation_with_mysql_relation(self, ops_test: OpsTest):
         """Test failure of addition of relational-db relation with mysql relation present."""
         # deploy mysql-k8s charm
+        # We should use `8.0/stable` once changes for 
+        # https://github.com/canonical/mysql-k8s-operator/issues/337 are published there.
         await ops_test.model.deploy(
             "mysql-k8s",
-            channel="8.0/stable",
+            channel="8.0/edge",
             config={"profile": "testing"},
             trust=True,
         )
