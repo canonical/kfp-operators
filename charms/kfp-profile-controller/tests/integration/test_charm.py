c15c4bb20e1c36679c9d38053a2a103b91cba8a2
Andrew Scribner <ca.scribner+1@gmail.com>
2024-04-11 10:51:33 -0400

    Update charms/kfp-profile-controller/tests/integration/test_charm.py

--- charms/kfp-profile-controller/tests/integration/test_charm.py
+++ charms/kfp-profile-controller/tests/integration/test_charm.py
@@ -250,7 +250,7 @@ async def test_sync_webhook(lightkube_client: lightkube.Client, profile: str):
 
 
 async def test_change_custom_images(ops_test: OpsTest, profile: str):
-    """Tests if the unit goes to active state after changing config with custom images."""
+    """Tests that updating images deployed to user Namespaces works as expected."""
     custom_images = {
         "visualization_server": CUSTOM_VISUALISATION_IMAGE,
         "frontend": CUSTOM_FRONTEND_IMAGE,
