38eb1cf74861894469c43d7ee7bd5d6ba6cf25a6
NohaIhab <noha.ihab@canonical.com>
2024-11-29 13:22:02 +0200

    skip: multi-line string to avoid noqa

--- charms/kfp-ui/src/components/pebble_components.py
+++ charms/kfp-ui/src/components/pebble_components.py
@@ -56,7 +56,13 @@ class MlPipelineUiPebbleService(PebbleServiceComponent):
                             # Must have the same value as the `keyFormat` specified in the
                             # `argo-workflow-controller-configmap` ConfigMap owned by
                             # the `argo-controller` charm.
-                            "ARGO_KEYFORMAT": "artifacts/{{workflow.name}}/{{workflow.creationTimestamp.Y}}/{{workflow.creationTimestamp.m}}/{{workflow.creationTimestamp.d}}/{{pod.name}}",  # noqa E501
+                            "ARGO_KEYFORMAT": (
+                                "artifacts/{{workflow.name}}/"
+                                "{{workflow.creationTimestamp.Y}}/"
+                                "{{workflow.creationTimestamp.m}}/"
+                                "{{workflow.creationTimestamp.d}}/"
+                                "{{pod.name}}"
+                            ),
                             # TODO: This should come from relation to kfp-profile-controller.
                             #  It is the name/port of the user-specific artifact accessor
                             "ARTIFACTS_SERVICE_PROXY_NAME": "ml-pipeline-ui-artifact",
