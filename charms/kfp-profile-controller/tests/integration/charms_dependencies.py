c4fb289c072b4cd6ae5b02d4620cb7ca75b00083
mvlassis <manos.vlassis@canonical.com>
2025-07-09 17:04:34 +0200

    Update channel for kfp charms on kfp-profile-controller

--- charms/kfp-profile-controller/tests/integration/charms_dependencies.py
+++ charms/kfp-profile-controller/tests/integration/charms_dependencies.py
@@ -4,7 +4,7 @@ from charmed_kubeflow_chisme.testing import CharmSpec
 
 ADMISSION_WEBHOOK = CharmSpec(charm="admission-webhook", channel="1.10/stable", trust=True)
 ISTIO_PILOT = CharmSpec(charm="istio-pilot", channel="1.24/stable", trust=True)
-KUBEFLOW_PROFILES = CharmSpec(charm="kubeflow-profiles", channel="1.24/stable", trust=True)
+KUBEFLOW_PROFILES = CharmSpec(charm="kubeflow-profiles", channel="1.10/stable", trust=True)
 METACONTROLLER_OPERATOR = CharmSpec(
     charm="metacontroller-operator", channel="4.11/stable", trust=True
 )
