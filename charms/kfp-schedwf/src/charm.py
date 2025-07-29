af3dff61a51ddf215762e2a992ad541433a606e9
MattiaSarti <nmattia1@gmail.com>
2025-07-29 10:19:40 +0200

    docs: add comments for serviceAccountToken

--- charms/kfp-schedwf/src/charm.py
+++ charms/kfp-schedwf/src/charm.py
@@ -68,6 +68,7 @@ class KfpSchedwf(CharmBase):
             depends_on=[self.leadership_gate],
         )
 
+        # creating a serviceAccountToken injected via a mounted projected volume:
         self.sa_token = self.charm_reconciler.add(
             component=SaTokenComponent(
                 charm=self,
