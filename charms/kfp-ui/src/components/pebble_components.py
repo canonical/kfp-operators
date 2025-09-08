957f60d035fb54dc48daf2ee7cf75c0b18d6dfae
Michal Hucko <michal.hucko@canonical.com>
2025-09-08 12:05:17 +0200

    feat: use `service-config.yaml` for `command` and `user` (#772)
    * feat: use service-config.yaml for  command and user

--- charms/kfp-ui/src/components/pebble_components.py
+++ charms/kfp-ui/src/components/pebble_components.py
@@ -24,6 +24,8 @@ class MlPipelineUiInputs:
     MINIO_SSL: str
     ML_PIPELINE_SERVICE_HOST: str
     ML_PIPELINE_SERVICE_PORT: str
+    USER: str
+    COMMAND: str
 
 
 class MlPipelineUiPebbleService(PebbleServiceComponent):
@@ -33,84 +35,89 @@ class MlPipelineUiPebbleService(PebbleServiceComponent):
             inputs: MlPipelineUiInputs = self._inputs_getter()
         except Exception as err:
             raise ValueError("Failed to get inputs for Pebble container.") from err
-        layer = Layer(
-            {
-                "services": {
-                    # TODO: should this be an attribute?  Or handled somehow else?
-                    self.service_name: {
-                        "override": "replace",
-                        "summary": "entry point for ml-pipeline-ui",
-                        # command should be updated each time we switch from upstream image to rock
-                        #  - upsstream: "command": "node dist/server.js ../client/ 3000"
-                        #  - rock: "command": "node /server/dist/server.js /client/ 3000"
-                        "command": "node /server/dist/server.js /client/ 3000",
-                        "startup": "enabled",
-                        "user": "_daemon_",  # This is needed only for rocks
-                        # TODO: are these still the correct settings?
-                        "environment": {
-                            "ALLOW_CUSTOM_VISUALIZATIONS": str(
-                                inputs.ALLOW_CUSTOM_VISUALIZATIONS
-                            ).lower(),
-                            "ARGO_ARCHIVE_ARTIFACTORY": "minio",
-                            "ARGO_ARCHIVE_BUCKETNAME": "mlpipeline",
-                            "ARGO_ARCHIVE_LOGS": inputs.ARGO_ARCHIVE_LOGS,
-                            # Must have the same value as the `keyFormat` specified in the
-                            # `argo-workflow-controller-configmap` ConfigMap owned by
-                            # the `argo-controller` charm.
-                            "ARGO_KEYFORMAT": (
-                                "artifacts/{{workflow.name}}/"
-                                "{{workflow.creationTimestamp.Y}}/"
-                                "{{workflow.creationTimestamp.m}}/"
-                                "{{workflow.creationTimestamp.d}}/"
-                                "{{pod.name}}"
-                            ),
-                            # TODO: This should come from relation to kfp-profile-controller.
-                            #  It is the name/port of the user-specific artifact accessor
-                            "ARTIFACTS_SERVICE_PROXY_NAME": "ml-pipeline-ui-artifact",
-                            "ARTIFACTS_SERVICE_PROXY_PORT": "80",
-                            "ARTIFACTS_SERVICE_PROXY_ENABLED": "true",
-                            "AWS_ACCESS_KEY_ID": "",
-                            "AWS_SECRET_ACCESS_KEY": "",
-                            "DISABLE_GKE_METADATA": inputs.DISABLE_GKE_METADATA,
-                            "ENABLE_AUTHZ": "true",
-                            "DEPLOYMENT": "KUBEFLOW",
-                            "FRONTEND_SERVER_NAMESPACE": inputs.FRONTEND_SERVER_NAMESPACE,
-                            "HIDE_SIDENAV": str(inputs.HIDE_SIDENAV).lower(),
-                            "HTTP_AUTHORIZATION_DEFAULT_VALUE": "",
-                            "HTTP_AUTHORIZATION_KEY": "",
-                            "HTTP_BASE_URL": "",
-                            "KUBEFLOW_USERID_HEADER": "kubeflow-userid",
-                            "KUBEFLOW_USERID_PREFIX": "",
-                            "METADATA_ENVOY_SERVICE_SERVICE_HOST": "localhost",
-                            "METADATA_ENVOY_SERVICE_SERVICE_PORT": "9090",
-                            "MINIO_ACCESS_KEY": f"{inputs.MINIO_ACCESS_KEY}",
-                            "MINIO_SECRET_KEY": inputs.MINIO_SECRET_KEY,
-                            "MINIO_HOST": inputs.MINIO_HOST,
-                            "MINIO_NAMESPACE": inputs.MINIO_NAMESPACE,
-                            "MINIO_PORT": inputs.MINIO_PORT,
-                            "MINIO_SSL": inputs.MINIO_SSL,
-                            "ML_PIPELINE_SERVICE_HOST": inputs.ML_PIPELINE_SERVICE_HOST,
-                            "ML_PIPELINE_SERVICE_PORT": inputs.ML_PIPELINE_SERVICE_PORT,
-                            "STREAM_LOGS_FROM_SERVER_API": "false",
-                            # TODO: Think there's a file here we should copy in.  Workload's logs
-                            #  show an error on start for this
-                            "VIEWER_TENSORBOARD_POD_TEMPLATE_SPEC_PATH": (
-                                "/etc/config/viewer-pod-template.json"
-                            ),
-                            "VIEWER_TENSORBOARD_TF_IMAGE_NAME": "tensorflow/tensorflow",
-                        },
-                    }
-                },
-                # TODO: Checks
-                # "checks": {
-                #     "kubeflow-profiles-get": {
-                #         "override": "replace",
-                #         "period": "30s",
-                #         "http": {"url": "http://localhost:8080/metrics"},
-                #     }
-                # },
-            }
-        )
+        # Build the layer
+        layer_dict = {
+            "services": {
+                # TODO: should this be an attribute?  Or handled somehow else?
+                self.service_name: {
+                    "override": "replace",
+                    "summary": "entry point for ml-pipeline-ui",
+                    # command in service-config.yaml should be updated upstream image/rock
+                    #  - upstream: "command": "node dist/server.js ../client/ 3000"
+                    #  - rock: "command": "node /server/dist/server.js /client/ 3000"
+                    "command": inputs.COMMAND,
+                    "startup": "enabled",
+                    # TODO: are these still the correct settings?
+                    "environment": {
+                        "ALLOW_CUSTOM_VISUALIZATIONS": str(
+                            inputs.ALLOW_CUSTOM_VISUALIZATIONS
+                        ).lower(),
+                        "ARGO_ARCHIVE_ARTIFACTORY": "minio",
+                        "ARGO_ARCHIVE_BUCKETNAME": "mlpipeline",
+                        "ARGO_ARCHIVE_LOGS": inputs.ARGO_ARCHIVE_LOGS,
+                        # Must have the same value as the `keyFormat` specified in the
+                        # `argo-workflow-controller-configmap` ConfigMap owned by
+                        # the `argo-controller` charm.
+                        "ARGO_KEYFORMAT": (
+                            "artifacts/{{workflow.name}}/"
+                            "{{workflow.creationTimestamp.Y}}/"
+                            "{{workflow.creationTimestamp.m}}/"
+                            "{{workflow.creationTimestamp.d}}/"
+                            "{{pod.name}}"
+                        ),
+                        # TODO: This should come from relation to kfp-profile-controller.
+                        #  It is the name/port of the user-specific artifact accessor
+                        "ARTIFACTS_SERVICE_PROXY_NAME": "ml-pipeline-ui-artifact",
+                        "ARTIFACTS_SERVICE_PROXY_PORT": "80",
+                        "ARTIFACTS_SERVICE_PROXY_ENABLED": "true",
+                        "AWS_ACCESS_KEY_ID": "",
+                        "AWS_SECRET_ACCESS_KEY": "",
+                        "DISABLE_GKE_METADATA": inputs.DISABLE_GKE_METADATA,
+                        "ENABLE_AUTHZ": "true",
+                        "DEPLOYMENT": "KUBEFLOW",
+                        "FRONTEND_SERVER_NAMESPACE": inputs.FRONTEND_SERVER_NAMESPACE,
+                        "HIDE_SIDENAV": str(inputs.HIDE_SIDENAV).lower(),
+                        "HTTP_AUTHORIZATION_DEFAULT_VALUE": "",
+                        "HTTP_AUTHORIZATION_KEY": "",
+                        "HTTP_BASE_URL": "",
+                        "KUBEFLOW_USERID_HEADER": "kubeflow-userid",
+                        "KUBEFLOW_USERID_PREFIX": "",
+                        "METADATA_ENVOY_SERVICE_SERVICE_HOST": "localhost",
+                        "METADATA_ENVOY_SERVICE_SERVICE_PORT": "9090",
+                        "MINIO_ACCESS_KEY": f"{inputs.MINIO_ACCESS_KEY}",
+                        "MINIO_SECRET_KEY": inputs.MINIO_SECRET_KEY,
+                        "MINIO_HOST": inputs.MINIO_HOST,
+                        "MINIO_NAMESPACE": inputs.MINIO_NAMESPACE,
+                        "MINIO_PORT": inputs.MINIO_PORT,
+                        "MINIO_SSL": inputs.MINIO_SSL,
+                        "ML_PIPELINE_SERVICE_HOST": inputs.ML_PIPELINE_SERVICE_HOST,
+                        "ML_PIPELINE_SERVICE_PORT": inputs.ML_PIPELINE_SERVICE_PORT,
+                        "STREAM_LOGS_FROM_SERVER_API": "false",
+                        # TODO: Think there's a file here we should copy in.  Workload's logs
+                        #  show an error on start for this
+                        "VIEWER_TENSORBOARD_POD_TEMPLATE_SPEC_PATH": (
+                            "/etc/config/viewer-pod-template.json"
+                        ),
+                        "VIEWER_TENSORBOARD_TF_IMAGE_NAME": "tensorflow/tensorflow",
+                    },
+                }
+            },
+            # TODO: Checks
+            # "checks": {
+            #     "kubeflow-profiles-get": {
+            #         "override": "replace",
+            #         "period": "30s",
+            #         "http": {"url": "http://localhost:8080/metrics"},
+            #     }
+            # },
+        }
+        # Change the value of user in `service-config.yaml`:
+        # - upstream: Leave string empty
+        # - rock: _daemon_
+        if inputs.USER:
+            layer_dict["services"][self.service_name]["user"] = inputs.USER
+
+        layer = Layer(layer_dict)
 
         logger.debug("computed layer as:")
         logger.debug(layer.to_dict())
