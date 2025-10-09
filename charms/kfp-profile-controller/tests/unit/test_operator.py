89cb315466f75fb89b6a27cae7659f295a36d251
Mattia Sarti <59971270+MattiaSarti@users.noreply.github.com>
2025-10-09 12:22:28 +0200

    feat: configure default pipeline root (#787)
    * feature: allow to update default pipeline root via charm config
    
    * test: update ConfigMap check on Metacontroller's webhook
    
    * fix: ensure the deafult Pebble service's environment is updated on config changes
    
    * fix: get event as charm attribute
    
    * fix: replace hyphens with underscores in event name
    
    * refactor: make testing more maintainable
    
    * docs: comment why default value
    
    * test: cover default config value and changes to it
    
    * fix: config naming style coherent with existing configs
    
    * style: compact arguments into single lines
    
    * test: simulated the necessary ConfigMap deletion
    
    * test: skip line length check for long link
    
    * docs: document necessary manual ConfigMap deletion
    
    * style: compact arguments into single lines
    
    * docs: fix grammar
    
    * test: simulate the necessary ConfigMap deletion after config updates
    
    * docs: make config note clearer
    
    * fix: update default config to actually reflect upstream
    
    * docs: fix doctring grammar
    
    * test: fix typo
    
    * test: assert exception raised for no ConfigMap
    
    * refactor: make tests more maintainable
    
    * refactor: make tests more maintainable
    
    * style: fix import order
    
    * fix: bypass lazy loading for Pytest's parametrization
    
    * style: fix multi-line parameters
    
    * build: switch to different package for lazy fixtures in Pytest
    
    * test: fix typo
    
    * test: fix test definition

--- charms/kfp-profile-controller/tests/unit/test_operator.py
+++ charms/kfp-profile-controller/tests/unit/test_operator.py
@@ -2,6 +2,7 @@
 # See LICENSE file for licensing details.
 
 import json
+from copy import deepcopy
 from pathlib import Path
 from unittest.mock import MagicMock, patch
 
@@ -14,17 +15,21 @@ from ops.testing import Harness
 from charm import (
     CONTROLLER_PORT,
     DISABLE_ISTIO_SIDECAR,
-    KFP_DEFAULT_PIPELINE_ROOT,
     KFP_IMAGES_VERSION,
     METADATA_GRPC_SERVICE_HOST,
     METADATA_GRPC_SERVICE_PORT,
     KfpProfileControllerOperator,
 )
 
+CONFIG_NAME_FOR_CUSTOM_IMAGES = "custom_images"
+CONFIG_NAME_FOR_DEFAULT_PIPELINE_ROOT = "default_pipeline_root"
+
 # Load the custom images from the JSON
 CUSTOM_IMAGES_PATH = Path("./src/default-custom-images.json")
 with CUSTOM_IMAGES_PATH.open() as f:
     custom_images = json.load(f)
+
+KFP_DEFAULT_PIPELINE_ROOT = ""
 
 MOCK_OBJECT_STORAGE_DATA = {
     "access-key": "access-key",
@@ -35,7 +40,7 @@ MOCK_OBJECT_STORAGE_DATA = {
     "secure": True,
 }
 
-EXPECTED_ENVIRONMENT = {
+EXPECTED_ENVIRONMENT_BY_DEFAULT = {
     "CONTROLLER_PORT": CONTROLLER_PORT,
     "DISABLE_ISTIO_SIDECAR": DISABLE_ISTIO_SIDECAR,
     "KFP_DEFAULT_PIPELINE_ROOT": KFP_DEFAULT_PIPELINE_ROOT,
@@ -181,11 +186,22 @@ def test_kubernetes_created_method(
     assert isinstance(harness.charm.kubernetes_resources.status, ActiveStatus)
 
 
+@pytest.mark.parametrize("do_update_config_for_default_pipeline_root", (False, True))
 def test_pebble_services_running(
-    harness, mocked_lightkube_client, mocked_kubernetes_service_patch
+    do_update_config_for_default_pipeline_root,
+    harness,
+    mocked_lightkube_client,
+    mocked_kubernetes_service_patch,
 ):
     """Test that if the Kubernetes Component is Active, the pebble services successfully start."""
     # Arrange
+    expected_environment = deepcopy(EXPECTED_ENVIRONMENT_BY_DEFAULT)
+    if do_update_config_for_default_pipeline_root:
+        updated_default_pipeline_root = "s3://whatever-s3-bucket/whatever/s3/path"
+        harness.update_config(
+            {CONFIG_NAME_FOR_DEFAULT_PIPELINE_ROOT: updated_default_pipeline_root}
+        )
+        expected_environment["KFP_DEFAULT_PIPELINE_ROOT"] = updated_default_pipeline_root
     harness.begin()
     harness.set_can_connect("kfp-profile-controller", True)
 
@@ -208,14 +224,14 @@ def test_pebble_services_running(
     assert service.is_running()
     # Assert the environment variables that are set from inputs are correctly applied
     environment = container.get_plan().services["kfp-profile-controller"].environment
-    assert environment == EXPECTED_ENVIRONMENT
+    assert environment == expected_environment
 
 
 def test_custom_images_config_with_incorrect_config(
     harness, mocked_lightkube_client, mocked_kubernetes_service_patch
 ):
     """Asserts that the unit goes to blocked on corrupted config input."""
-    harness.update_config({"custom_images": "{"})
+    harness.update_config({CONFIG_NAME_FOR_CUSTOM_IMAGES: "{"})
     harness.begin()
 
     assert isinstance(harness.model.unit.status, BlockedStatus)
@@ -226,7 +242,9 @@ def test_custom_images_config_with_incorrect_images_names(
     harness, mocked_lightkube_client, mocked_kubernetes_service_patch
 ):
     """Asserts that the unit goes to blocked on incorrect images names in the config input."""
-    harness.update_config({"custom_images": yaml.dump({"name1": "image1", "name2": "image2"})})
+    harness.update_config(
+        {CONFIG_NAME_FOR_CUSTOM_IMAGES: yaml.dump({"name1": "image1", "name2": "image2"})}
+    )
     harness.begin()
 
     assert isinstance(harness.model.unit.status, BlockedStatus)
