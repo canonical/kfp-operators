c109d4bd714f81facdfaab590b449b822556881b
Dario Faccin <dario.faccin@canonical.com>
2025-12-10 16:07:10 +0100

    feat(kfp-viz): run workload and charm as unprivileged user
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- charms/kfp-viz/src/components/pebble_components.py
+++ charms/kfp-viz/src/components/pebble_components.py
@@ -1,7 +1,6 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-import dataclasses
 import logging
 
 from charmed_kubeflow_chisme.components.pebble_component import PebbleServiceComponent
@@ -10,21 +9,9 @@ from ops.pebble import Layer
 logger = logging.getLogger(__name__)
 
 
-@dataclasses.dataclass
-class KfpVizInputs:
-    """Defines the required inputs for KfpVizInputs."""
-
-    USER: str
-
-
 class KfpVizPebbleService(PebbleServiceComponent):
     def get_layer(self) -> Layer:
         """Pebble configuration layer for ml-pipeline-visualizationserver."""
-
-        try:
-            inputs: KfpVizInputs = self._inputs_getter()
-        except Exception as err:
-            raise ValueError("Failed to get inputs for Pebble container.") from err
 
         layer_dict = {
             "services": {
@@ -47,11 +34,6 @@ class KfpVizPebbleService(PebbleServiceComponent):
             },
         }
 
-        # Change the value of user in `service-config.yaml`:
-        # - upstream: Leave string empty
-        # - rock: _daemon_
-        if inputs.USER:
-            layer_dict["services"][self.service_name]["user"] = inputs.USER
         layer = Layer(layer_dict)
 
         return layer
