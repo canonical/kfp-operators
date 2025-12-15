ec9b24c0c3428720bdd9e872722d1c86325f8ea9
Manos Vlassis <57320708+mvlassis@users.noreply.github.com>
2025-12-15 11:17:48 +0100

    feat: Use jubilant in bundle integration tests (#810)
    * Use jubilant in bundle integration tests
    
    * Use jubilant in bundle integration tests
    
    * Use jubilant in bundle integration tests
    
    * Use jubilant in bundle integration tests
    
    * Use jubilant in bundle integration tests
    
    * Add linting to bundle tests
    
    * Update linting
    
    * Update linting
    
    * Add bundle-lint job
    
    * Update ci.yaml
    
    * Update ci.yaml
    
    * Update ci.yaml
    
    * Update ci.yaml
    
    * Update ci.yaml
    
    * Update ci.yaml
    
    * Update ci.yaml
    
    * Remove unnecessary comments
    
    * Add section about running tests in CONTRIBUTING.md
    
    * Add --no-deploy option
    
    * Fix linting
    
    * Add section about running tests in CONTRIBUTING.md
    
    * Rename function from  to
    
    * Add abort_on_failure
    
    * Delete pipeline properly
    
    * Don't exit on errors
    
    * Wait for all agents to be idle

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,22 +2,20 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 """Conftest for integration tests."""
+import logging
 import subprocess
 import time
 from pathlib import Path
 
-from helpers.k8s_resources import apply_manifests
-from kfp_globals import *
-
+import jubilant
 import kfp
-import kfp_server_api
 import lightkube
 import pytest
 import yaml
+from _pytest.config.argparsing import Parser
+from helpers.k8s_resources import apply_manifests
 from lightkube import codecs
 from lightkube.generic_resource import create_global_resource
-
-from _pytest.config.argparsing import Parser
 
 basedir = Path("./").absolute()
 
@@ -28,6 +26,69 @@ KUBEFLOW_PROFILE_NAMESPACE = "kubeflow-user-example-com"
 PROFILE_FILE_PATH = f"{basedir}/tests/integration/profile/profile.yaml"
 PROFILE_FILE = yaml.safe_load(Path(PROFILE_FILE_PATH).read_text())
 KUBEFLOW_USER_NAME = PROFILE_FILE["spec"]["owner"]["name"]
+
+WAIT_TIMEOUT = 20 * 60
+
+log = logging.getLogger(__name__)
+
+
+def pytest_configure(config):
+    # Custom markers
+    config.addinivalue_line("markers", "deploy: mark test as a deployment test")
+    config.addinivalue_line(
+        "markers", "abort_on_fail: abort the entire test session if this test fails"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--no-deploy"):
+        skip_deploy = pytest.mark.skip(reason="skipped because --no-deploy was passed")
+        for item in items:
+            # If the test has the @pytest.mark.deploy marker, skip it
+            if "deploy" in item.keywords:
+                item.add_marker(skip_deploy)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # Yield control to allow the test to run and generate a report
+    outcome = yield
+    report = outcome.get_result()
+
+    # If the test failed during execution
+    if report.when == "call" and report.failed:
+        # Check if the test function had the custom marker
+        if item.get_closest_marker("abort_on_fail"):
+            log.warning(f"\n[ABORT] Test '{item.name}' failed. Aborting session.")
+            # Gracefully stop the session
+            item.session.shouldstop = True
+
+
+@pytest.fixture(scope="module")
+def juju(request: pytest.FixtureRequest):
+    keep_models = bool(request.config.getoption("--keep-models"))
+    model_name = request.config.getoption("--model")
+
+    def print_debug_log(juju_instance: jubilant.Juju):
+        if request.session.testsfailed:
+            print(f"[DEBUG] Fetching debug log for model: {juju_instance.model}")
+            log = juju_instance.debug_log(limit=1000)
+            print(log, end="")
+
+    if model_name:
+        juju_instance = jubilant.Juju(model=model_name)
+        juju_instance.wait_timeout = WAIT_TIMEOUT
+        try:
+            yield juju_instance
+        finally:
+            print_debug_log(juju_instance)
+    else:
+        with jubilant.temp_model(keep=keep_models) as juju_instance:
+            juju_instance.wait_timeout = WAIT_TIMEOUT
+            try:
+                yield juju_instance
+            finally:
+                print_debug_log(juju_instance)
 
 
 @pytest.fixture(scope="session")
@@ -50,9 +111,7 @@ def forward_kfp_ui():
 def apply_profile(lightkube_client):
     """Apply a Profile simulating a user."""
     # Create a Viewer namespaced resource
-    profile_class_resource = create_global_resource(
-        group="kubeflow.org", version="v1", kind="Profile", plural="profiles"
-    )
+    create_global_resource(group="kubeflow.org", version="v1", kind="Profile", plural="profiles")
 
     # Apply Profile first
     apply_manifests(lightkube_client, PROFILE_FILE_PATH)
@@ -91,25 +150,30 @@ def lightkube_client() -> lightkube.Client:
 
 def pytest_addoption(parser: Parser):
     parser.addoption(
+        "--keep-models",
+        action="store_true",
+        default=False,
+        help="keep temporarily-created models",
+    )
+    parser.addoption(
+        "--model",
+        action="store",
+        help="Juju model to use; if not provided, a new model "
+        "will be created for each test which requires one",
+    )
+    parser.addoption(
+        "--no-deploy",
+        action="store_true",
+        default=False,
+        help="Don't deploy any charms before testing",
+    )
+    parser.addoption(
         "--bundle",
         default="./tests/integration/bundles/bundle.yaml.j2",
         help="Path to bundle file to use as the template for tests.  This must include all charms"
         "built by this bundle, where the locally built charms will replace those specified. "
         "This is useful for testing this bundle against different external dependencies. "
-        "An example file is in ./tests/integration/bundles/bundle.yaml",
-    )
-    parser.addoption(
-        "--no-build",
-        action="store_true",
-        help="Whether the charms in this repository should be built locally and used"
-        "to render the bundle definition template."
-        "If set to False, the integration tests will be run against charms in Charmhub.",
-    )
-    parser.addoption(
-        "--charmcraft-clean",
-        action="store_true",
-        help="Whether to run charmcraft clean and delete lxc instances created by charmcraft."
-        "It defaults to False."
+        "An example file is in ./tests/integration/bundles/bundle.yaml.j2",
     )
     parser.addoption(
         "--charms-path",
--- tests/integration/pipelines/pipeline_container_no_input.py
+++ tests/integration/pipelines/pipeline_container_no_input.py
@@ -11,10 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-
-from kfp import compiler
-from kfp import dsl
+from kfp import compiler, dsl
 
 
 @dsl.container_component
