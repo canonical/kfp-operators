2e31034af5cc1a3f757d1efb5b7eedfb4dc5d9eb
Kimonas Sotirchos <kimonas.sotirchos@canonical.com>
2026-01-27 14:24:51 +0200

    feat: integrate scheduled workflows with ambient  (#835)
    Closes https://github.com/canonical/kfp-operators/issues/806
    
    1. The charm now has an optional `kfp-api` relation
    2. If the above relation is established then this service will be used for talking to the KFP API instead of `ml-pipeline`
    3. Integrate the charm with the service mesh, so that there will be mTLS when it talks with KFP API in ambient
    
    **NOTE**: This PR doesn't introduce any ambient specific tests. We do this to avoid duplicating the test with the bundle tests. We'll test the whole KFP and it's integration with ambient in a dedicated PR.

--- charms/kfp-schedwf/src/components/pebble_component.py
+++ charms/kfp-schedwf/src/components/pebble_component.py
@@ -1,11 +1,20 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
+import dataclasses
 import logging
 
 from charmed_kubeflow_chisme.components.pebble_component import PebbleServiceComponent
 from ops.pebble import Layer
 
 logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class KfpSchedwfPebbleServiceComponentInputs:
+    """Defines the required inputs for the Pebble Component."""
+
+    KFP_API_SERVICE: str
+    KFP_API_GRPC_PORT: int
 
 
 class KfpSchedwfPebbleService(PebbleServiceComponent):
@@ -32,6 +41,11 @@ class KfpSchedwfPebbleService(PebbleServiceComponent):
         """
         logger.info("PebbleServiceComponent.get_layer executing")
 
+        try:
+            inputs: KfpSchedwfPebbleServiceComponentInputs = self._inputs_getter()
+        except Exception as err:
+            raise ValueError(f"{self.name}: inputs are not correctly provided") from err
+
         # NOTE: to check exactly how we are supposed to reuse rocks' predefined
         # pebble services, this could work, but I have to check if there are no
         # other edge cases were the layer is defined somewhere else and it is
@@ -45,9 +59,11 @@ class KfpSchedwfPebbleService(PebbleServiceComponent):
                     "override": "replace",
                     "summary": "scheduled workflow controller service",
                     "startup": "enabled",
-                    "command": "/bin/controller --logtostderr=true"
-                    ' --namespace=""'
-                    f" --logLevel={self.log_level}",
+                    "command": "/bin/controller --logtostderr=true "
+                    '--namespace="" '
+                    f"--logLevel={self.log_level} "
+                    f"--mlPipelineAPIServerName={inputs.KFP_API_SERVICE} "
+                    f"--mlPipelineServiceGRPCPort={inputs.KFP_API_GRPC_PORT}",
                     "environment": self.environment,
                 }
             },
