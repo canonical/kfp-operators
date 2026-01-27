0ad1573ac87dd39f3d2685db8e49197ac4d27e48
Noha Ihab <49988746+NohaIhab@users.noreply.github.com>
2026-01-27 12:11:41 +0200

    feat: integrate `kfp-viz` with ambient mesh (#829)
    * feat: integrate kfp-viz with ambient mesh
    ---------
    
    Signed-off-by: Noha Ihab <49988746+NohaIhab@users.noreply.github.com>

---
+++ charms/kfp-viz/tests/integration/test_charm_ambient.py
@@ -0,0 +1,95 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+from pathlib import Path
+
+import pytest
+import yaml
+from charmed_kubeflow_chisme.testing import (
+    GRAFANA_AGENT_APP,
+    assert_logging,
+    assert_security_context,
+    deploy_and_assert_grafana_agent,
+    deploy_and_integrate_service_mesh_charms,
+    generate_container_securitycontext_map,
+    get_pod_names,
+)
+from lightkube import Client
+from pytest_operator.plugin import OpsTest
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+CHARM_ROOT = "."
+APP_NAME = METADATA["name"]
+CONTAINERS_SECURITY_CONTEXT_MAP = generate_container_securitycontext_map(METADATA)
+
+log = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="session")
+def lightkube_client() -> Client:
+    client = Client(field_manager=APP_NAME)
+    return client
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy_with_relations(ops_test: OpsTest, request: pytest.FixtureRequest):
+    image_path = METADATA["resources"]["oci-image"]["upstream-source"]
+    resources = {"oci-image": image_path}
+    # Keep the option to run the integration tests locally
+    # by building the charm and then deploying
+    entity_url = (
+        await ops_test.build_charm("./")
+        if not (entity_url := request.config.getoption("--charm-path"))
+        else entity_url
+    )
+    await ops_test.model.deploy(
+        entity_url=entity_url,
+        application_name=APP_NAME,
+        resources=resources,
+        trust=True,
+    )
+
+    await deploy_and_integrate_service_mesh_charms(
+        APP_NAME, ops_test.model, relate_to_ingress_route_endpoint=False
+    )
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="active",
+        raise_on_blocked=False,
+        raise_on_error=False,
+        timeout=10 * 60,
+    )
+
+    # Deploying grafana-agent-k8s and add all relations
+    await deploy_and_assert_grafana_agent(
+        ops_test.model, APP_NAME, metrics=False, logging=True, dashboard=False
+    )
+
+
+async def test_logging(ops_test: OpsTest):
+    """Test logging is defined in relation data bag."""
+    app = ops_test.model.applications[GRAFANA_AGENT_APP]
+    await assert_logging(app)
+
+
+@pytest.mark.parametrize("container_name", list(CONTAINERS_SECURITY_CONTEXT_MAP.keys()))
+async def test_container_security_context(
+    ops_test: OpsTest,
+    lightkube_client: Client,
+    container_name: str,
+):
+    """Test container security context is correctly set.
+
+    Verify that container spec defines the security context with correct
+    user ID and group ID.
+    """
+    pod_name = get_pod_names(ops_test.model.name, APP_NAME)[0]
+    assert_security_context(
+        lightkube_client,
+        pod_name,
+        container_name,
+        CONTAINERS_SECURITY_CONTEXT_MAP,
+        ops_test.model.name,
+    )
