da291571f765762152c8b0d3b01e1275312a436f
NohaIhab <noha.ihab@canonical.com>
2026-02-05 18:48:25 +0200

    chore: update terraform outputs to include ambient mesh relations

--- charms/kfp-api/terraform/outputs.tf
+++ charms/kfp-api/terraform/outputs.tf
@@ -4,18 +4,22 @@ output "app_name" {
 
 output "provides" {
   value = {
-    kfp_api           = "kfp-api",
-    metrics_endpoint  = "metrics-endpoint",
     grafana_dashboard = "grafana-dashboard",
+    kfp_api           = "kfp-api",
+    kfp_api_grpc      = "kfp-api-grpc",
+    metrics_endpoint  = "metrics-endpoint",
+    provide_cmr_mesh  = "provide-cmr-mesh"
   }
 }
 
 output "requires" {
   value = {
-    mysql          = "mysql",
-    relational_db  = "relational-db",
-    object_storage = "object-storage",
-    kfp_viz        = "kfp-viz",
-    logging        = "logging",
+    kfp_viz          = "kfp-viz",
+    logging          = "logging",
+    mysql            = "mysql",
+    object_storage   = "object-storage",
+    relational_db    = "relational-db",
+    require_cmr_mesh = "require-cmr-mesh",
+    service_mesh     = "service-mesh"
   }
 }
