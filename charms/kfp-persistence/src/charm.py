ae8a8758646a4dfecd5119b186c3808f277b1529
Enrico Deusebio <enrico.deusebio@canonical.com>
2026-02-09 15:52:17 +0100

    [KF-8438] (kfp-persistence) Remove statefulness from charm configuration

--- charms/kfp-persistence/src/charm.py
+++ charms/kfp-persistence/src/charm.py
@@ -42,9 +42,7 @@ class KfpPersistenceOperator(CharmBase):
 
         # Storage
         self._container_name = next(iter(self.meta.containers))
-        _container_meta = self.meta.containers[self._container_name]
-        _storage_name = next(iter(_container_meta.mounts))
-        self._secrets_storage_path = Path(_container_meta.mounts[_storage_name].location)
+        self._secrets_storage_path = Path("/var/run/secrets/kubeflow/tokens")
         # Charm logic
         self.charm_reconciler = CharmReconciler(self)
 
