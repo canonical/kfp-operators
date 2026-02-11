74687df8b64cf54f7aea4e9008ca40828766d277
Noha Ihab <49988746+NohaIhab@users.noreply.github.com>
2026-02-11 10:56:01 +0200

    chore: update terraform outputs to include ambient mesh relations (#849)

--- charms/kfp-profile-controller/terraform/outputs.tf
+++ charms/kfp-profile-controller/terraform/outputs.tf
@@ -3,12 +3,16 @@ output "app_name" {
 }
 
 output "provides" {
-  value = {}
+  value = {
+    provide_cmr_mesh = "provide-cmr-mesh"
+  }
 }
 
 output "requires" {
   value = {
-    object_storage = "object-storage",
-    logging        = "logging",
+    logging          = "logging",
+    object_storage   = "object-storage",
+    require_cmr_mesh = "require-cmr-mesh",
+    service_mesh     = "service-mesh"
   }
 }
